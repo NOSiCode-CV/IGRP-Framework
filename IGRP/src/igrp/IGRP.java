f002598c1fee8e2fb3170dd3e2eb7511d22bd6f9
Yma <Yma@192.168.56.1>
2017-04-12 14:17:33 -0100

    Update class IGRP

--- IGRP/src/igrp/IGRP.java
+++ IGRP/src/igrp/IGRP.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/IGRP")
 public class IGRP extends HttpServlet {
-	private static final long serialVersionUID = 102831973239L;
+	private static final long serialVersionUID = 01L;
        
     /**
      * @see HttpServlet#HttpServlet()
