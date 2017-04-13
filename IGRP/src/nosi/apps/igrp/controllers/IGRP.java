40bb05b76a30bfee60a4ded72c1968c502b1307c
Yma <Yma@192.168.56.1>
2017-04-13 12:02:36 -0100

    Override AbstractField

--- IGRP/src/nosi/apps/igrp/controllers/IGRP.java
+++ IGRP/src/nosi/apps/igrp/controllers/IGRP.java
@@ -7,6 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import nosi.core.gui.components.FormXML;
+import nosi.core.gui.components.ViewXML;
+import nosi.core.gui.fields.CheckBoxListField;
+import nosi.core.gui.fields.EmailField;
+import nosi.core.gui.fields.Field;
+import nosi.core.gui.fields.HiddenField;
+import nosi.core.gui.fields.ListField;
+import nosi.core.gui.fields.RadioListField;
+import nosi.core.gui.fields.TextField;
+
 /**
  * Servlet implementation class IGRP
  */
@@ -27,8 +37,53 @@ public class IGRP extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		/*response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.getWriter().append("Param at: ").append(request.getParameter("r"));
+		*/
+		FormXML form = new FormXML("form_1");
+		FormXML form2 = new FormXML("form_2");
+		ViewXML view = new ViewXML("view_1");
+		Field nome = new TextField("nome");
+		
+		nome.setValue("Ima");
+		
+		Field morada = new TextField("morada");
+		morada.setValue("VN");
+		
+		Field hidden = new HiddenField("hidden");
+		hidden.setValue(1);
+		
+		Field select = new ListField("select");
+		select.addOption("Option 1", 1);
+		select.addOption("Option 2", 2);
+		
+		Field radiolist = new RadioListField("radiolist");
+		radiolist.addOption("Check 1", 1);
+		radiolist.addOption("Check 2", 2);
+		
+
+		Field checkboxlist = new CheckBoxListField("checkboxlist");
+		checkboxlist.addOption("Check 1", 1);
+		checkboxlist.addOption("Check 2", 2);
+		
+		Field email = new EmailField("email");
+		email.setValue("ima@gmail.com");
+		
+		
+		form.addField(nome);
+		form.addField(morada);
+		form.addField(hidden);
+		form.addField(email);
+		
+		form2.addField(select);
+		form2.addField(radiolist);
+		form2.addField(checkboxlist);
+		
+		view.addField(email);
+		view.addField(nome);
+		response.getWriter().append(form.toString());
+		response.getWriter().append(form2.toString());
+		response.getWriter().append(view.toString());
 	}
 
 	/**
