6dceb7335eca0142def68396d046d8e29cb95a93
Isaias1994GitHub <joaquim199410@gmail.com>
2017-04-24 17:37:49 -0100

    Class ACTION, CONFIG, CONFIG_ENV and MENU done.

---
+++ IGRP/src/nosi/webapps/igrp/dao/Config_env.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package nosi.webapps.igrp.dao;
+
+/**
+ * @author Isaias.Nunes
+ *
+ */
+public class Config_env {
+
+	/**
+	 * 
+	 */
+	public Config_env() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
