e6c1d75111f7667ef648bcca2e35464ff2c0f5ef
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-04-26 12:08:04 -0100

    Alteracao do caminho relativo.

--- IGRP/src/nosi/core/config/Config.java
+++ IGRP/src/nosi/core/config/Config.java
@@ -6,8 +6,8 @@ import nosi.core.xml.XMLWritter;
 
 public class Config {
 	
-	public  static final String ROOT_PATH = "D:/git/IGRP-Framework/IGRP/src";
-	public Config() {
+	public  static final String ROOT_PATH = "D:/git/IGRP-Framework/IGRP/src/";
+	public Config() { 
 		// TODO Auto-generated constructor stub
 	}
 
