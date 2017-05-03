f318f3c7ffdfee51d6588a2e0aefec925d7f2c8b
Isaias1994GitHub <joaquim199410@gmail.com>
2017-05-03 08:32:49 -0100

    Conflito

--- IGRP/src/nosi/core/config/Config.java
+++ IGRP/src/nosi/core/config/Config.java
@@ -8,14 +8,12 @@ import nosi.core.xml.XMLWritter;
 
 public class Config {
 	
-<<<<<<< HEAD
 	/*public static final String PATH_CLASS = "C:/Users/isaias.nunes/git/IGRP-Framework/IGRP/src/";
 	public static final String PATH_XSL = "C:/Users/isaias.nunes/git/IGRP-Framework/IGRP/WebContent/images/IGRP/IGRP2.3/app/";
 	
 	public static final String LINK_IMG = "/IGRP/images/IGRP/IGRP2.3";*/
 	//""
-=======
->>>>>>> branch 'master' of https://github.com/IMarcelF/IGRP-Framework.git
+
 	public static String TITLE = "";
 	public static String target = "";
 	
