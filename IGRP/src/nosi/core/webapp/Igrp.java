ef6bfeccf73ae81d555b114835aedee4c09be581
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-05-16 16:33:09 -0100

    Bug fixed ... AuthenicationFilter, LoginController, User init

--- IGRP/src/nosi/core/webapp/Igrp.java
+++ IGRP/src/nosi/core/webapp/Igrp.java
@@ -179,9 +179,9 @@ public class Igrp {
 	}
 	
 	private void load(Map<String, String> m){ // load and apply some dependency injection ...
-		if(!Permission.isPermition(this.currentAppName,this.currentPageName,this.currentActionName))
+		/*if(!Permission.isPermition(this.currentAppName,this.currentPageName,this.currentActionName))
 			throw new PermissionException("Nao tem permissao para aceder esta aplicacao");
-			
+			*/
 		String controllerPath = m.get("controllerPath");
 		String actionName = m.get("actionName");
 		
