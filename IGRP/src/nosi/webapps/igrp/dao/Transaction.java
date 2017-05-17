f6d72f47a2961996011178589c295c068695b0f3
Yma <Yma@192.168.56.1>
2017-05-17 12:21:19 -0100

    Lista de Transacoes

--- IGRP/src/nosi/webapps/igrp/dao/Transaction.java
+++ IGRP/src/nosi/webapps/igrp/dao/Transaction.java
@@ -122,6 +122,7 @@ public class Transaction implements RowDataGateway {
 	@Override
 	public boolean update() {
 		try{
+			con.setAutoCommit(true);
 			PreparedStatement st = con.prepareStatement("UPDATE glb_t_transaction SET "
 					+ "code=?, "
 					+ "descr=?, "
@@ -134,7 +135,7 @@ public class Transaction implements RowDataGateway {
 			st.setInt(4, this.status);
 	        st.executeUpdate();
 	        st.close();
-			
+			return true;
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
@@ -148,6 +149,7 @@ public class Transaction implements RowDataGateway {
 			st.setInt(1, this.id);
 			st.executeUpdate();
 			st.close();
+			return true;
 		}catch(SQLException e){
 			e.printStackTrace();
 		}
@@ -176,13 +178,5 @@ public class Transaction implements RowDataGateway {
 		}
 		return lista.toArray();
 	}
-
-	@Override
-	public String toString() {
-		return "Transaction [id=" + id + ", code=" + code + ", descr=" + descr + ", env_fk=" + env_fk + ", status="
-				+ status + "]";
-	}
-	
-	
 
 }
--- IGRP/src/nosi/webapps/igrp/pages/transaccao/TransaccaoController.java
+++ IGRP/src/nosi/webapps/igrp/pages/transaccao/TransaccaoController.java
@@ -1,8 +1,11 @@
 
 package nosi.webapps.igrp.pages.transaccao;
 import nosi.core.webapp.Controller;
+import nosi.core.webapp.Igrp;
 import nosi.webapps.igrp.dao.Application;
 import nosi.webapps.igrp.dao.Organization;
+import nosi.webapps.igrp.dao.Transaction;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -10,22 +13,23 @@ import java.util.ArrayList;
 public class TransaccaoController extends Controller {		
 
 	public void actionIndex() throws IOException{
-		Transaccao model = new Transaccao();
+		Transaccao model = new Transaccao();		
+		ArrayList<Transaccao.Table_1> table_1 = new ArrayList<>();
+		for(Object obj:new Transaction().getAll()){
+			Transaction t = (Transaction) obj;
+			Transaccao.Table_1 table = new Transaccao().new Table_1();
+			table.setCodigo(t.getCode());
+			table.setDescricao(t.getDescr());
+			table.setEstado(t.getStatus()==1?"Ativo":"Inativo");
+			table_1.add(table);
+		}		
+		
 		TransaccaoView view = new TransaccaoView(model);
+		view.title = "Gestao de Transação";
 		view.aplicacao.setValue(new Application().getListApps());
 		view.organica.setValue(new Organization().getListOrganizations());
-		ArrayList<Transaccao.Table_1> table_1 = new ArrayList<>();
-		
-		for(Object obj:new Organization().getAll()){
-			Organization org = (Organization) obj;
-			Transaccao.Table_1 t = new Transaccao().new Table_1();
-			t.setCodigo(org.getCode());
-			t.setDescricao(org.getName());
-			t.setEstado(org.getStatus()==1?"Ativo":"Inativo");
-			table_1.add(t);
-		}
-		view.title = "Gestao de Transação";
-		model.setTable_1(table_1);
+		view.table_1.addData(table_1);
+		view.codigo.setParam(true);
 		this.renderView(view);
 	}
 
@@ -42,11 +46,35 @@ public class TransaccaoController extends Controller {
 	}
 	
 	public void actionEliminar() throws IOException{
-			this.redirect("igrp","Transaccao","index");
+		String id = Igrp.getInstance().getRequest().getParameter("id");
+		Transaction t = new Transaction();
+		t.setId(Integer.parseInt(id));
+		if(t.delete())
+			Igrp.getInstance().getFlashMessage().addMessage("success","Operação efetuada com sucesso");
+		else
+			Igrp.getInstance().getFlashMessage().addMessage("error","Falha ao tentar efetuar esta operação");
+		this.redirect("igrp","transacao","index");
 	}
 	
 	public void actionAlterar_estado() throws IOException{
-			this.redirect("igrp","Transaccao","index");
+		String code = Igrp.getInstance().getRequest().getParameter("codigo");
+		Transaction t = new Transaction();
+		t.setCode(code);
+		if(((Transaction) t.getOne()).getCode()!=null){
+			t = (Transaction) t.getOne();
+			if(t.getStatus()==0){
+				System.out.println("alterar para 1");
+				t.setStatus(1);
+			}else if(t.getStatus()==1){
+				System.out.println("alterar para 0");
+				t.setStatus(0);
+			}
+			if(t.update())	
+				Igrp.getInstance().getFlashMessage().addMessage("success","Operação efetuada com sucesso");
+			else
+				Igrp.getInstance().getFlashMessage().addMessage("error","Falha ao tentar efetuar esta operação");
+		}
+		this.redirect("igrp","Transaccao","index");
 	}
 	
 }
