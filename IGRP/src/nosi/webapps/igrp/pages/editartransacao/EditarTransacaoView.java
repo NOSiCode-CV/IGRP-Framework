f3a4d699c366f70b13332d3762558e1741da766e
Yma <Yma@192.168.56.1>
2017-05-18 16:12:03 -0100

    Fixes bug on getMyMenu and implement Edit Transaction

---
+++ IGRP/src/nosi/webapps/igrp/pages/editartransacao/EditarTransacaoView.java
@@ -0,0 +1,64 @@
+/*-------------------------*/
+
+/*Create View*/
+
+package nosi.webapps.igrp.pages.editartransacao;
+import nosi.core.webapp.View;
+import nosi.core.gui.components.*;
+import nosi.core.gui.fields.*;
+import nosi.core.config.Config; 
+
+public class EditarTransacaoView extends View {
+	public String title = "Editar Transacao";		
+	
+	public Field codigo;
+	public Field descricao;
+	public Field aplicacao;
+	public Field status;
+	public Field id;
+	public Field status_check;
+	public IGRPForm form_1;
+
+	public IGRPToolsBar toolsbar_1;
+	public IGRPButton btn_gravar;
+	public IGRPButton btn_voltar;
+	public EditarTransacaoView(EditarTransacao model){			
+		form_1 = new IGRPForm("form_1");
+		codigo = new TextField(model,"codigo");
+		codigo.setLabel("Codigo");
+		codigo.propertie().add("name","p_codigo").add("type","text").add("maxlength","100").add("required","true").add("change","false").add("readonly","false").add("disabled","false").add("placeholder","").add("right","false");
+		descricao = new TextField(model,"descricao");
+		descricao.setLabel("Descricao");
+		descricao.propertie().add("name","p_descricao").add("type","text").add("maxlength","150").add("required","true").add("change","false").add("readonly","false").add("disabled","false").add("placeholder","").add("right","false");
+		aplicacao = new ListField(model,"aplicacao");
+		aplicacao.setLabel("Aplicacao");
+		aplicacao.propertie().add("name","p_aplicacao").add("type","select").add("multiple","false").add("maxlength","30").add("required","true").add("change","false").add("disabled","false").add("right","false");
+		status = new CheckBoxField(model,"status");
+		status.setLabel("Ativo?");
+		status.propertie().add("name","p_status").add("type","checkbox").add("maxlength","30").add("required","false").add("change","false").add("readonly","false").add("disabled","false").add("right","false").add("check","true");
+		id = new HiddenField(model, "p_id");
+		toolsbar_1 = new IGRPToolsBar("toolsbar_1");
+		btn_gravar = new IGRPButton("Gravar","igrp","EditarTransacao","gravar","submit","default|fa-save","","");
+		btn_gravar.propertie.add("type","specific").add("code","").add("rel","gravar");
+		btn_voltar = new IGRPButton("Voltar","igrp","EditarTransacao","voltar","_self","default|fa-angle-right","","");
+		btn_voltar.propertie.add("type","specific").add("code","").add("rel","voltar");
+		
+	}
+		
+	@Override
+	public void render(){
+		Config.TITLE = this.title;
+
+		form_1.addField(codigo);
+		form_1.addField(descricao);
+		form_1.addField(aplicacao);
+		form_1.addField(status);
+		form_1.addField(id);
+
+		toolsbar_1.addButton(btn_gravar);
+		toolsbar_1.addButton(btn_voltar);
+		this.addToPage(form_1);
+		this.addToPage(toolsbar_1);
+	}
+}
+/*-------------------------*/
--- IGRP/src/nosi/webapps/igrp/pages/pesquisarmenu/PesquisarMenuController.java
+++ IGRP/src/nosi/webapps/igrp/pages/pesquisarmenu/PesquisarMenuController.java
@@ -76,28 +76,30 @@ public class PesquisarMenuController extends Controller {
 		XMLWritter xml_menu = new XMLWritter();
 		xml_menu.startElement("menus");
 		HashMap<String,String> menus = new HashMap<>();
-		for(Object obj:new Menu().getMyMenu()){
-			Menu m = (Menu) obj;
-			XMLWritter xml = new XMLWritter();
-			xml.startElement("submenu");
-			xml.writeAttribute("title", m.getDescr());
-			xml.writeAttribute("id",""+m.getSelf_id());
-			xml.setElement("link", "webapps?r="+m.getLink());
-			xml.setElement("title", m.getDescr());
-			xml.setElement("target", m.getTarget());
-			xml.setElement("id",""+ m.getSelf_id());
-			xml.setElement("status",""+ m.getStatus());
-			xml.endElement();
-			if(menus.get(m.getCode()+":::"+m.getId())!=null){
-				xml.addXml(menus.get(m.getCode()+":::"+m.getId()).toString());
+		if(Igrp.getInstance().getUser().isAuthenticated()){
+			for(Object obj:new Menu().getMyMenu()){
+				Menu m = (Menu) obj;
+				XMLWritter xml = new XMLWritter();
+				xml.startElement("submenu");
+				xml.writeAttribute("title", m.getDescr());
+				xml.writeAttribute("id",""+m.getSelf_id());
+				xml.setElement("link", "webapps?r="+m.getLink());
+				xml.setElement("title", m.getDescr());
+				xml.setElement("target", m.getTarget());
+				xml.setElement("id",""+ m.getSelf_id());
+				xml.setElement("status",""+ m.getStatus());
+				xml.endElement();
+				if(menus.get(m.getCode()+":::"+m.getId())!=null){
+					xml.addXml(menus.get(m.getCode()+":::"+m.getId()).toString());
+				}
+				menus.put(m.getCode()+":::"+m.getId(),xml.toString());
 			}
-			menus.put(m.getCode()+":::"+m.getId(),xml.toString());
-		}
-		for(Entry<String, String> m:menus.entrySet()){
-			xml_menu.startElement("menu");
-			xml_menu.setElement("title", m.getKey().substring(0,m.getKey().toString().indexOf(":::")));
-			xml_menu.addXml(m.getValue());
-			xml_menu.endElement();
+			for(Entry<String, String> m:menus.entrySet()){
+				xml_menu.startElement("menu");
+				xml_menu.setElement("title", m.getKey().substring(0,m.getKey().toString().indexOf(":::")));
+				xml_menu.addXml(m.getValue());
+				xml_menu.endElement();
+			}
 		}
 		xml_menu.endElement();
 		return Igrp.getInstance().getResponse().getWriter().append(xml_menu.toString());
