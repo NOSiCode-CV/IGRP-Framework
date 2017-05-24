2a4af1e2a6a66370f8ede1ce64e5a339f8c937ed
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-05-24 10:07:55 -0100

    just test it

--- IGRP/src/nosi/teste/MainTest.java
+++ IGRP/src/nosi/teste/MainTest.java
@@ -1,16 +1,11 @@
 package nosi.teste;
 
-import java.util.*;
+import java.io.*;
 
 public class MainTest {
 	
 	public static void main(String[] args) {
-		/* URL location = MainTest.class.getProtectionDomain().getCodeSource().getLocation();
-	        System.out.println(location.getFile());
-		final String dir = System.getProperty("user.dir");
-		System.out.println("current dir = " + dir);
-		*/
-		
+		File file = new File("");
 	}
 
 }
