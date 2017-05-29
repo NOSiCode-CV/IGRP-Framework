04756f8958b645321cf290e9174ffd92b243563c
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-05-29 09:24:56 -0100

    just commit

--- IGRP/src/nosi/teste/MainTest.java
+++ IGRP/src/nosi/teste/MainTest.java
@@ -2,7 +2,14 @@ package nosi.teste;
 
 public class MainTest {
 	
+	public void func1(int cod, String name){
+		
+	}
+	
 	public static void main(String[] args){
+		
+		Thread t = new Thread();
+		
 		
 	}
 
