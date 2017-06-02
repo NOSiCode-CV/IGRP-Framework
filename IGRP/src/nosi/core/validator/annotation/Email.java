02ec5c27d4b384b4fb0747b926d2bcd80566c22a
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-06-02 11:40:33 -0100

    Implementation of RequiredValidator and EmailValidator.

--- IGRP/src/nosi/core/validator/annotation/Email.java
+++ IGRP/src/nosi/core/validator/annotation/Email.java
@@ -1,9 +1,21 @@
 package nosi.core.validator.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Marcel Iekiny
  * May 29, 2017
  */
-
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface Email {
-
+	String []acceptedScenarios() default {};
+	String []ignoredScenarios() default {};
+	boolean allowEmpty() default true;
+	boolean skipOnError() default false;
+	String message() default "";
+	boolean allowName() default false;
 }
--- IGRP/src/nosi/teste/MyTeste.java
+++ IGRP/src/nosi/teste/MyTeste.java
@@ -1,9 +1,14 @@
 package nosi.teste;
 
+import nosi.webapps.igrp.pages.login.Login;
+
 public class MyTeste {
 
 	public static void main(String []args){
-		
+		Login model = new Login();
+		model.setUser("iekjd2323ini.fernandes@nosi.cv.pt");
+		model.validate();
+		System.out.println(model.hasErrors("user"));
 	}
 	
 }
