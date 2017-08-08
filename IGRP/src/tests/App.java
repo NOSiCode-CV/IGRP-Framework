12e0bca0195a4548a82059a07f58700f548ef5df
Marcel Iekiny <iekinyfernandes@gmail.com>
2017-08-08 11:15:06 -0100

    last commit ever

--- IGRP/src/tests/App.java
+++ IGRP/src/tests/App.java
@@ -2,12 +2,16 @@ package tests;
 
 import java.util.List;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+import nosi.webapps.agenda.dao.Entidade;
 import nosi.webapps.agenda.helper.RestRequestHelper;
 /**
  * @author Marcel Iekiny
@@ -21,36 +25,32 @@ public final class App {
 		makePostRequest2();
 	}
 	
-	
 	public static void makePostRequest2() {
 		ClientConfig config = new DefaultClientConfig();
 		 
         Client client = Client.create(RestRequestHelper.applySslSecurity(config));
         
-        String url = RestRequestHelper.baseUrl + "/user";
+        String url = RestRequestHelper.baseUrl + "/entidade";
         
         WebResource resource = client.resource(url);
        
-        User user = new User();
-		user.setUsername("IMF");
-		user.setPassword_hash("djfhjdhfjd");
-		user.setAuth_key("deeuriwui");
-		user.setStatus(10);
-		user.setPassword_reset_token("NULL");
-		user.setEmail("iekinyfernandes3@gmail.com");
-		user.setUpdated_at(12345);
-		user.setCreated_at(45344);
-		user.setFuncionario(1);
+        Entidade entidade = new Entidade();
+        entidade.setNome_entidade("Entidade2");
+        entidade.setAplicacao("Aplicacao1");
+        entidade.setEstado("Fechado");
 		
-		String content = RestRequestHelper.createJsonPostData("_postuser", user);
+		String content = RestRequestHelper.createJsonPostData("_postentidade", entidade);
 		
         ClientResponse response = resource.accept(MediaType.APPLICATION_JSON).type("application/json")
         		.post(ClientResponse.class, content);
 
    	 	String jsonResult = response.getEntity(String.class);
    	 	
+   	 	//System.out.println(jsonResult);
+   	 	
         if(response.getStatus() == 200) {
         	System.out.println("Ok");
+        	System.out.println(RestRequestHelper.extractGeneratedKeyEntryId(jsonResult) + "");
         }
         else {
        	 System.out.println(RestRequestHelper.convertToDefaultFault(jsonResult).getFaultstring());
@@ -114,6 +114,5 @@ public final class App {
        	 //System.out.println(RestRequestHelper.convertToDefaultFault(jsonResult));
         }
        client.destroy();
-	}
-	
+	}	
 }
