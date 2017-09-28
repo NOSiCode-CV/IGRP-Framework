03c578be1fe7038a8b28c50034ad2998e5fcad33
Isaias1994GitHub <joaquim199410@gmail.com>
2017-09-28 17:18:12 -0100

    Interface Execucao Tarefas

---
+++ IGRP/src/nosi/webapps/igrp/pages/execucaotarefas/ExecucaoTarefasController.java
@@ -0,0 +1,137 @@
+/*-------------------------*/
+
+/*Create Controller*/
+
+package nosi.webapps.igrp.pages.execucaotarefas;
+/*---- Import your packages here... ----*/
+import nosi.core.webapp.Controller;
+import java.io.IOException;
+import nosi.core.webapp.Response;
+
+/*---- End ----*/
+
+public class ExecucaoTarefasController extends Controller {		
+
+
+	public Response actionIndex() throws IOException{
+		/*---- Insert your code here... ----*/
+		ExecucaoTarefas model = new ExecucaoTarefas();
+		ExecucaoTarefasView view = new ExecucaoTarefasView(model);
+		return this.renderView(view);
+		/*---- End ----*/
+	}
+
+
+	public Response actionPesquisar_tarefa() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionPesquisar_colaborador() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionPesquisar_estatistica() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionPesquisar_button_minhas_tarefas() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionPesquisar_button_disponiveis() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionTransferir_tarefa() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionDetalhes_tarefa() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionDetalhes_processo() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionAlterar_prioridade_tarefa() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionVer_estatistica() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionVer_detalhes() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionEnviar_msg() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionExecutar_button_minha_tarefas() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionDetalhes_processos_button_minha_tarefas() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionLeberar_tarefa_button_minha_tarefas() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+
+	public Response actionAssumir_button_tabela() throws IOException{
+		/*---- Insert your code here... ----*/
+		return this.redirect("igrp","ExecucaoTarefas","index");
+		/*---- End ----*/
+	}
+	
+	/*---- Insert your actions here... ----*//*---- End ----*/
+}
