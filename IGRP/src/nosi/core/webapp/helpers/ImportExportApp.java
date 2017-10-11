2a496dd405c3262393b2ffa6f3882677869c123d
Isaias1994GitHub <joaquim199410@gmail.com>
2017-10-11 17:01:25 -0100

    gerando a tabela ImportExport

--- IGRP/src/nosi/core/webapp/helpers/ImportExportApp.java
+++ IGRP/src/nosi/core/webapp/helpers/ImportExportApp.java
@@ -1,8 +1,18 @@
 package nosi.core.webapp.helpers;
+
+import nosi.core.config.Config;
+import nosi.core.webapp.Igrp;
+import nosi.webapps.igrp.dao.Action;
+
 /**
  * Isaias.Nunes
  * Oct 11, 2017
  */
 public class ImportExportApp {
+	
+	public void export() {
+		
+	}
+	
 	
 }
