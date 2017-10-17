a871ebec43ca200a344485cdcc74423b92a721e6
Isaias1994GitHub <joaquim199410@gmail.com>
2017-10-17 09:00:20 -0100

    Import Arquivo Jar(pagina)

--- IGRP/src/nosi/webapps/igrp/pages/importarquivo/ImportArquivoController.java
+++ IGRP/src/nosi/webapps/igrp/pages/importarquivo/ImportArquivoController.java
@@ -8,7 +8,6 @@ import nosi.core.webapp.Controller;
 import nosi.core.webapp.Igrp;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import nosi.core.webapp.Response;
@@ -32,15 +31,22 @@ public class ImportArquivoController extends Controller {
 
 	public Response actionImport() throws IOException{
 		/*---- Insert your code here... ----*/
-		Map<String , String> un_jar_files = JarUnJarFile.getJarFiles("C:\\Users\\isaias.nunes\\Downloads\\ListaEnv.jar");
+		Map<String , String> un_jar_files = JarUnJarFile.getJarFiles("C:\\Users\\isaias.nunes\\Downloads\\ListaPage.jar");
 		boolean status_compile = false;
 		for(Map.Entry<String, String> un_jar_file:un_jar_files.entrySet()) {
+			FileHelper.save("C:\\Users\\isaias.nunes\\Downloads", un_jar_file.getKey(), un_jar_file.getValue());
+		}
+		for(Map.Entry<String, String> un_jar_file:un_jar_files.entrySet()) {
 			if(un_jar_file.getKey().contains(".java")) {
-				//FileHelper.save("C:\\Users\\isaias.nunes\\Downloads", un_jar_file.getKey(), un_jar_file.getValue());
-			    status_compile = CompilerHelper.compile("C:\\Users\\isaias.nunes\\Downloads", un_jar_file.getKey()); 
+				
+				
+				//status_compile = CompilerHelper.compile("C:\\Users\\isaias.nunes\\Downloads", un_jar_file.getKey());
+				status_compile = CompilerHelper.compile("C:\\Users\\isaias.nunes\\Downloads", "ListaPage" + ".java");
+				status_compile = CompilerHelper.compile("C:\\Users\\isaias.nunes\\Downloads", "ListaPage"  + "View.java");
+				status_compile = CompilerHelper.compile("C:\\Users\\isaias.nunes\\Downloads", "ListaPage" + "Controller.java");
 			}
 			
-			if(un_jar_files.containsKey(".xml")) {
+			if(un_jar_files.containsKey("Config.xml")) {
 				
 			}
 			//System.out.println(un_jar_file.getKey());
@@ -59,14 +65,21 @@ public class ImportArquivoController extends Controller {
 	/*---- Insert your actions here... ----*//*---- End ----*/
 	
 	
-	public static void main(String [] args) throws IOException {
+	
+	
+	
+	
+	
+	
+	
+	//public static void main(String [] args) throws IOException {
 		
-		Map<String , String> un_jar_files = JarUnJarFile.getJarFiles("C:\\Users\\isaias.nunes\\Downloads\\Dominio.jar");
+		//Map<String , String> un_jar_files = JarUnJarFile.getJarFiles("C:\\Users\\isaias.nunes\\Downloads\\ListaPage.jar");
 		//boolean status_compile = false;
 		
-		for(Map.Entry<String, String> un_jar_file:un_jar_files.entrySet()) {
+	//	for(Map.Entry<String, String> un_jar_file:un_jar_files.entrySet()) {
 			//if(un_jar_file.getKey().contains(".xml")) {
-				FileHelper.save("C:\\Users\\isaias.nunes\\Downloads", un_jar_file.getKey(), un_jar_file.getValue());
+		//		FileHelper.save("C:\\Users\\isaias.nunes\\Downloads", un_jar_file.getKey(), un_jar_file.getValue());
 			    //status_compile = CompilerHelper.compile("C:\\Users\\isaias.nunes\\Downloads", un_jar_file.getKey()); 
 			//System.out.println(un_jar_file.getKey());	
 			//System.out.println(un_jar_file.getValue()); 
@@ -77,16 +90,20 @@ public class ImportArquivoController extends Controller {
 			}*/
 			//System.out.println(un_jar_file.getKey());
 			//System.out.println(un_jar_file.getValue());
-		}
-		
-		
+	//	}
+			
 		//System.out.println(status_compile); 
+		/*LinkedHashMap<String, String> frutas = new LinkedHashMap<String, String>();
+		frutas.put("a", "Apple");
+		frutas.put("c", "caju");
+		frutas.put("b", "Banana");
+		System.out.println(frutas);
+		
+		
+		System.out.println(frutas);
+		
+	
 	}
-	
-	
-	
-	
-	
-	
+	*/
 	
 }
