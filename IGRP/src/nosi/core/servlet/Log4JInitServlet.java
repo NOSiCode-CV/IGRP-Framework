817a0f63be876f4e644d3b5e26b07cc5697c1c30
Angelo.Correia <Angelo.Correia@169.254.44.1>
2017-12-06 09:04:29 -0100

    implementação log4j e db-migrator (Maven plugin)

---
+++ IGRP/src/nosi/core/servlet/Log4JInitServlet.java
@@ -0,0 +1,43 @@
+package nosi.core.servlet;
+
+import java.io.File;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * Servlet implementation class Log4JInitServlet
+ * Ângelo Correia
+ */
+@WebServlet("/Log4JInitServlet")
+public class Log4JInitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	public void init(ServletConfig config) throws ServletException {
+		
+		String log4jLocation = config.getInitParameter("log4j-properties-location");
+
+		ServletContext sc = config.getServletContext();
+		if (log4jLocation == null) {
+			System.err.println("No log4j-properties-location init param, so initializing log4j with BasicConfigurator");
+			BasicConfigurator.configure();
+		} else {
+			String webAppPath = sc.getRealPath("/");
+			String log4jProp = webAppPath + log4jLocation;
+			File newlogfile = new File(log4jProp);
+			if (newlogfile.exists()) {
+
+				PropertyConfigurator.configure(log4jProp);
+			} else {
+				System.err.println(" " + log4jProp + " file not found, so initializing log4j with BasicConfigurator");
+				BasicConfigurator.configure();
+			}
+		}
+		super.init(config);
+	}
+
+}
