afeb02c7e127e2776f5a7b90234a55c2c271f015
Iekiny Marcel <Iekiny Marcel@10.73.97.27>
2017-12-12 09:44:56 -0100

    Bug fixed in all filters

--- IGRP/src/nosi/core/listener/BasicListener.java
+++ IGRP/src/nosi/core/listener/BasicListener.java
@@ -5,20 +5,16 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import nosi.base.ActiveRecord.PersistenceUtils;
-import nosi.core.webapp.Igrp;
-import nosi.core.webapp.ThreadLocalIgrpFactory;
 /**
  * Marcel Iekiny
  * Nov 23, 2017
  */
 @WebListener
-public class DataBase implements ServletContextListener {
+public class BasicListener implements ServletContextListener {
 
-    public DataBase() {} 
+    public BasicListener() {} 
 
     public void contextDestroyed(ServletContextEvent arg0)  {
-    	ThreadLocalIgrpFactory t = (ThreadLocalIgrpFactory)Igrp.getInstance().getAppInstance();
-    	t.cleanUp();
     	PersistenceUtils.destroy(); 
     }
 
