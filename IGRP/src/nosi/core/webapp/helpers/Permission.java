e92a66890332641809cb2cf2ae7327154229e5cb
Emanuel <Emanuel@192.168.56.1>
2017-12-12 15:31:17 -0100

    Fixed bug on permition

--- IGRP/src/nosi/core/webapp/helpers/Permission.java
+++ IGRP/src/nosi/core/webapp/helpers/Permission.java
@@ -71,9 +71,11 @@ public class Permission {
 		((User)Igrp.getInstance().getUser().getIdentity()).setAplicacao(app);
 		((User)Igrp.getInstance().getUser().getIdentity()).setProfile(profType);
 		((User)Igrp.getInstance().getUser().getIdentity()).setOrganica(org);
-		Igrp.getInstance().getRequest().getSession().setAttribute("igrp.env", app.getDad());
-		Igrp.getInstance().getRequest().getSession().setAttribute("igrp.prof", profType.getId());
-		Igrp.getInstance().getRequest().getSession().setAttribute("igrp.org", org.getId());
+		if(Igrp.getInstance().getRequest().getSession()!=null && app!=null) {
+			Igrp.getInstance().getRequest().getSession().setAttribute("igrp.env", app.getDad());
+			Igrp.getInstance().getRequest().getSession().setAttribute("igrp.prof", profType.getId());
+			Igrp.getInstance().getRequest().getSession().setAttribute("igrp.org", org.getId());
+		}
 	}
 	
 	public static String getCurrentEnv() {
