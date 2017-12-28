b53bb0e90c14e05d4bfc1dd689e111157c41819e
Marcos Miguel Barros Brito <marcosmb.brito@gmail.com>
2017-12-28 12:47:14 -0100

    Correção de bug duplicacao no readfile

--- IGRP/src/nosi/core/webapp/helpers/FileHelper.java
+++ IGRP/src/nosi/core/webapp/helpers/FileHelper.java
@@ -218,7 +218,7 @@ public class FileHelper {
 			    DataInputStream in = new DataInputStream(is);   
 			    BufferedReader d = new BufferedReader(new InputStreamReader(in));
 			    while((line=d.readLine())!=null){
-			    	code.append(new String(line.getBytes(),"UTF-8"));
+//			    	code.append(new String(line.getBytes(),"UTF-8"));
 			    	code.append(line);
 			    	code.append(ls);
 			    }
@@ -244,7 +244,7 @@ public class FileHelper {
 		    BufferedReader d = new BufferedReader(new InputStreamReader(in));
 		    while((line=d.readLine())!=null){
 		    	
-		    	code.append(new String(line.getBytes(),"UTF-8"));
+		    	code.append(new String(line.getBytes(),"UTF-8"));		
 		    	code.append(ls);
 		    }
 		    is.close();
