144bce7413482a482264346bef9161709926f65c
Isaías Nunes <Isaas Nunes@10.73.96.47>
2018-01-11 11:18:38 -0100

    List Process and application

--- IGRP/src/nosi/webapps/igrp_studio/pages/bpmndesigner/BPMNDesignerController.java
+++ IGRP/src/nosi/webapps/igrp_studio/pages/bpmndesigner/BPMNDesignerController.java
@@ -6,7 +6,12 @@ package nosi.webapps.igrp_studio.pages.bpmndesigner;
 /*----#START-PRESERVED-AREA(PACKAGES_IMPORT)----*/
 import nosi.core.webapp.Controller;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import nosi.core.webapp.Response;
+import nosi.core.webapp.activit.rest.ProcessDefinitionService;
+import nosi.webapps.igrp.dao.Application;
 
 /*----#END-PRESERVED-AREA----*/
 
@@ -17,7 +22,16 @@ public class BPMNDesignerController extends Controller {
 		/*----#START-PRESERVED-AREA(INDEX)----*/
 		BPMNDesigner model = new BPMNDesigner();
 		BPMNDesignerView view = new BPMNDesignerView(model);
-		view.btn_gravar.setLink("");
+		view.env_fk.setValue(new Application().getListApps());
+		
+		List<BPMNDesigner.Gen_table> data = new ArrayList<>();
+		for(ProcessDefinitionService process: new ProcessDefinitionService().getProcessDefinitionsAtivos()){
+			BPMNDesigner.Gen_table processo = new BPMNDesigner.Gen_table();
+			processo.setId(process.getId());
+			processo.setTitle(process.getName());
+			data.add(processo);
+		}
+		view.gen_table.addData(data);
 		return this.renderView(view);
 		/*----#END-PRESERVED-AREA----*/
 	}
@@ -25,21 +39,21 @@ public class BPMNDesignerController extends Controller {
 
 	public Response actionGravar() throws IOException{
 		/*----#START-PRESERVED-AREA(GRAVAR)----*/
-		return this.redirect("igrp_studio","BPMNDesigner","index");
+		return this.redirect("igrp","BPMNDesigner","index");
 		/*----#END-PRESERVED-AREA----*/
 	}
 	
 
 	public Response actionPublicar() throws IOException{
 		/*----#START-PRESERVED-AREA(PUBLICAR)----*/
-		return this.redirect("igrp_studio","BPMNDesigner","index");
+		return this.redirect("igrp","BPMNDesigner","index");
 		/*----#END-PRESERVED-AREA----*/
 	}
 	
 
 	public Response actionExporar_imagem() throws IOException{
 		/*----#START-PRESERVED-AREA(EXPORTAR_IMAGEM)----*/
-		return this.redirect("igrp_studio","BPMNDesigner","index");
+		return this.redirect("igrp","BPMNDesigner","index");
 		/*----#END-PRESERVED-AREA----*/
 	}
 	
