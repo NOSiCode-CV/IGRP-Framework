8ea44da5b0f0552e3e47db3883ecd1f1e6b26f88
Marcos Miguel Barros Brito <marcosmb.brito@gmail.com>
2018-01-18 15:28:27 -0100

    [<CDATA  não há mais necessidade de usar CDATA
    Config - Delete de replace amp; nos URLs

--- IGRP/src/main/java/nosi/core/config/Config.java
+++ IGRP/src/main/java/nosi/core/config/Config.java
@@ -167,7 +167,7 @@ public class Config {
 		return getConfig().get("version")!=null? getConfig().get("version").toString():"1.0";
 	}
 	public static String getLinkSileMenu(){
-		return "webapps?r=igrp/pesquisar-menu/myMenu&amp;dad="+Permission.getCurrentEnv();
+		return "webapps?r=igrp/pesquisar-menu/myMenu&dad="+Permission.getCurrentEnv();
 	}
 	public static String getLinkTopMenu(){
 		return "webapps?r=igrp/pesquisar-menu/topMenu";//getConfig().get("link_top_menu")!=null? getConfig().get("link_top_menu").toString():"";
@@ -194,8 +194,7 @@ public class Config {
 	public static String getResolveUrl(String app,String page,String action){
 //		HttpServletRequest req = Igrp.getInstance().getRequest();
 		
-		String url = "webapps?r="+app+"/"+page+"/"+(action.contains("&amp;")?action:action.replace("&", "&amp;"))+"&amp;dad="+Permission.getCurrentEnv();
-//		String url = req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort()+req.getContextPath()+"/webapps?r="+app+"/"+page+"/"+action+"&amp;dad="+Permission.getCurrentEnv();
+		String url = "webapps?r="+app+"/"+page+"/"+action+"dad="+Permission.getCurrentEnv();
 
 		return url;
 	}
