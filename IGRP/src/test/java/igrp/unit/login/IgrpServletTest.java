95b85ed0a6ed4183649ea0821570cd7f12913ad3
Angelo Correia <angelo.correia@nosi.cv>
2018-02-15 09:43:57 -0100

    inactive glassfish and active tom ee

--- IGRP/src/test/java/igrp/unit/login/IgrpServletTest.java
+++ IGRP/src/test/java/igrp/unit/login/IgrpServletTest.java
@@ -8,7 +8,9 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import nosi.core.servlet.IgrpServlet;
-@Ignore
+import nosi.core.webapp.FlashMessage;
+import nosi.core.webapp.Igrp;
+//@Ignore
 public class IgrpServletTest {
 
 	
@@ -16,16 +18,33 @@ public class IgrpServletTest {
     public void testServlet() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);       
         HttpServletResponse response = mock(HttpServletResponse.class);    
-
+                
+        
         when(request.getParameter("username")).thenReturn("demo");
-        when(request.getParameter("password")).thenReturn("1");
+        when(request.getParameter("password")).thenReturn("demo");
+        
+       
+    
+        request.getSession().setAttribute("_flash", "teste");
+       
+        //when(request.getParameter("_flash")).thenReturn("demo");
 
         StringWriter stringWriter = new StringWriter();
         PrintWriter writer = new PrintWriter(stringWriter);
         when(response.getWriter()).thenReturn(writer);
-
-        new IgrpServlet().doGet(request, response);
-
+        
+        IgrpServlet igrpservlet = new IgrpServlet();
+        //FlashMessage.setMessage("sd", "sd");
+        
+       // Igrp igrp = Igrp.getInstance(); //.init(null, request, response);
+      // igrp.getRequest().
+        
+       // new IgrpServlet().doGet(request, response);
+       
+    //    igrp.init(servlet, request, response);
+      //  igrp.doGet(request, response);
+        nosi.core.webapp.Igrp.getInstance().init(igrpservlet, request, response).run();        
+        
         verify(request, atLeast(1)).getParameter("username"); // only if you want to verify username was called...
         writer.flush(); // it may not have been flushed yet...
         assertTrue(stringWriter.toString().contains("My expected string"));
