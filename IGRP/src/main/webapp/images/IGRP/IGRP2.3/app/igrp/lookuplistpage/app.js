a753124cde1c97c2b719d83411edb89e3c726be6
William.Lima <William.Lima@NOSI-DDS-01.gov.cv>
2018-04-05 15:50:12 -0100

    Componentes formlist reordable

--- IGRP/src/main/webapp/images/IGRP/IGRP2.3/app/igrp/lookuplistpage/app.js
+++ IGRP/src/main/webapp/images/IGRP/IGRP2.3/app/igrp/lookuplistpage/app.js
@@ -1,5 +1,51 @@
 (function(){
 	
-	console.log('OK');
+	function SetSortable(){
+		
+		var formlist = $('#formlist_1'),
+		
+			tdCount  = $('tbody>tr:first-child>td',formlist).filter(function(){
+				return $(this).css('display') != 'none';
+			}).length;
+		
+		$('td[item-name="t_descricao_componente"]').each(function(i,td){
+			
+			var sortHandler = $(
+				'<div class="fl-sort-handler" style="position:absolute;right:8px;top:50%;margin-top:-9px;cursor:move"><i class="fa fa-bars"></i></div>'
+			);
+			
+			$(td).css({
+				'padding-right':'30px',
+				'position'     :'relative'
+			}).append(sortHandler);
+			
+		})
+		
+		$('tbody',formlist).sortable({
+	    	items : ">tr",
+	    	containment: "parent",
+	    	handle  : ".fl-sort-handler",
+	        axis: "y",
+	        tolerance: "pointer",
+	        helper: function(){
+	        	return $('<tr/>')
+	        },
+	        start:function(e,ui){
+	        	if(ui.placeholder.find('td').length > tdCount ){
+	        		ui.placeholder.find('td').each(function(i,td){
+	        			if(i >= tdCount)
+	        				$(td).hide();
+	        		});
+	        	}
+	        }
+	    });
+	}
+	
+	$.IGRP.on('init',function(){
+		
+		SetSortable();
+	    
+	    
+	});
 	
 })();
