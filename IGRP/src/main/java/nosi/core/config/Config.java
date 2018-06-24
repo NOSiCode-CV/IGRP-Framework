2ba8ea3eb0e4d32fb0891629ee36cf619a0da875
Marcos Miguel Barros Brito <marcosmb.brito@gmail.com>
2018-06-24 15:55:54 -0100

    v.180624

--- IGRP/src/main/java/nosi/core/config/Config.java
+++ IGRP/src/main/java/nosi/core/config/Config.java
@@ -165,7 +165,7 @@ public class Config {
 	}
 
 	public String getFooterName(){
-		return getConfig().get("footer_name")!=null? getConfig().get("footer_name").toString():"2018 - Copyright NOSi v.180620";
+		return getConfig().get("footer_name")!=null? getConfig().get("footer_name").toString():"2018 - Copyright NOSi v.180624";
 	}
 	public String getWelcomeNote(){
 		return getConfig().get("welcome_note")!=null? getConfig().get("welcome_note").toString():"Ola";
