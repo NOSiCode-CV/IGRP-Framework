f6270f0ff8ba78e2f0e6c36effdbf3fad3f7c134
ymaPereira <emanueld.pereira2@gmail.com>
2018-08-08 11:42:47 -0100

    driver sql server

--- IGRP/src/main/java/nosi/base/ActiveRecord/HibernateUtils.java
+++ IGRP/src/main/java/nosi/base/ActiveRecord/HibernateUtils.java
@@ -179,42 +179,78 @@ public class HibernateUtils {
 
 	public static String getDriver(String type) {
 		switch (type.toLowerCase()) {
-		case "h2":
-			return "org.h2.Driver";
-		case "mysql":
-			return "com.mysql.jdbc.Driver";
-		case "postgresql":
-			return "org.postgresql.Driver";
-		case "oracle":
-			return "oracle.jdbc.driver.OracleDriver";
+			case "h2":
+				return "org.h2.Driver";
+			case "mysql":
+				return "com.mysql.jdbc.Driver";
+			case "postgresql":
+				return "org.postgresql.Driver";
+			case "oracle":
+				return "oracle.jdbc.driver.OracleDriver";
+			case "mssql":
+				return "com.Microsoft.Sqlserver.Jdbc.SQLServerDriver";
+			case "hsqldb":
+				return "org.Hsqldb.JdbcDriver";
+			case "sybase":
+				return "com.Sybase.Jdbc3.Jdbc.SybDriver";
+			case "derby":
+				return "org.Apache.Derby.Jdbc.EmbeddedDriver";
+			case "ibm":
+				return "com.Ibm.Db2.Jcc.DB2Driver";
+			case "informix":
+				return "com.Informix.Jdbc.IfxDriver";
 		}
 		return "org.h2.Driver";
 	}
 
 	public static String getUrl(String type, String host, String port, String db_name) {
 		switch (type) {
-		case "h2":
-			return host.equalsIgnoreCase("mem") ? ("jdbc:h2:" + host + ":" + db_name): ("jdbc:h2:" + host + "/" + db_name);
-		case "mysql":
-			return "jdbc:mysql://" + host + ":" + port + "/" + db_name;
-		case "postgresql":
-			return "jdbc:postgresql://" + host + ":" + port + "/" + db_name;
-		case "oracle":
-			return "jdbc:oracle:thin:@" + host + ":" + port + "/" + db_name;
+			case "h2":
+				return host.equalsIgnoreCase("mem") ? ("jdbc:h2:" + host + ":" + db_name): ("jdbc:h2:" + host + "/" + db_name);
+			case "mysql":
+				return "jdbc:mysql://" + host + ":" + port + "/" + db_name;
+			case "postgresql":
+				return "jdbc:postgresql://" + host + ":" + port + "/" + db_name;
+			case "oracle":
+				return "jdbc:oracle:thin:@" + host + ":" + port + "/" + db_name;
+			case "mssql":
+				return "jdbc:sqlserver://" + host + ":" + port + ";databaseName=" + db_name;
+			case "hsqldb":
+				return "org.Hsqldb.JdbcDriver";
+			case "sybase":
+				return "com.Sybase.Jdbc3.Jdbc.SybDriver";
+			case "derby":
+				return "org.Apache.Derby.Jdbc.EmbeddedDriver";
+			case "ibm":
+				return "com.Ibm.Db2.Jcc.DB2Driver";
+			case "informix":
+				return "com.Informix.Jdbc.IfxDriver";
 		}
 		return "";
 	}
 
 	public static String getHibernateDialect(String type) {
 		switch (type.toLowerCase()) {
-		case "h2":
-			return "org.hibernate.dialect.H2Dialect";
-		case "mysql":
-			return "org.hibernate.dialect.MySQL5InnoDBDialect";
-		case "postgresql":
-			return "org.hibernate.dialect.PostgreSQLDialect";
-		case "oracle":
-			return "org.hibernate.dialect.OracleDialect";
+			case "h2":
+				return "org.hibernate.dialect.H2Dialect";
+			case "mysql":
+				return "org.hibernate.dialect.MySQL5InnoDBDialect";
+			case "postgresql":
+				return "org.hibernate.dialect.PostgreSQLDialect";
+			case "oracle":
+				return "org.hibernate.dialect.OracleDialect";
+			case "mssql":
+				return "org.hibernate.dialect.SQLServerDialect";
+			case "hsqldb":
+				return "org.hibernate.dialect.HSQLDialect";
+			case "sybase":
+				return "org.hibernate.dialect.SybaseDialect";
+			case "derby":
+				return "org.hibernate.dialect.DerbyDialect";
+			case "ibm":
+				return "org.hibernate.dialect.DB2Dialect";
+			case "informix":
+				return "org.hibernate.dialect.InformixDialect";
 		}
 		return "";
 	}
