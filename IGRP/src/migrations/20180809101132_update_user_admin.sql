a557a9f4044ec5ec4885d857f312efa337a70d53
Isaias1994GitHub <joaquim199410@gmail.com>
2018-08-09 15:16:04 -0100

    small changes...

--- IGRP/src/migrations/20180809101132_update_user_admin.sql
+++ IGRP/src/migrations/20180809101132_update_user_admin.sql
@@ -1,4 +1,4 @@
 update public.tbl_user 
-set pass_hash = 'ae1986a53724bc3a28a63af48b3cd391a795a3c6c9e2583c00b94d68308554c7'
-set user_name = 'admin@nosi.cv'
+set pass_hash = 'ae1986a53724bc3a28a63af48b3cd391a795a3c6c9e2583c00b94d68308554c7',
+user_name = 'admin@nosi.cv'
 where id = 0;
