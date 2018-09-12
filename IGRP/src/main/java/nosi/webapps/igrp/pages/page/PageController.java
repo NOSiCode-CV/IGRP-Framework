d2713367166bd79240f19c9ebcf66fafa89a4e66
Marcos Miguel Barros Brito <marcosmb.brito@gmail.com>
2018-09-12 10:38:51 -0100

    refs #1973 closes

--- IGRP/src/main/java/nosi/webapps/igrp/pages/page/PageController.java
+++ IGRP/src/main/java/nosi/webapps/igrp/pages/page/PageController.java
@@ -150,7 +150,7 @@ public class PageController extends Controller {
 				// ______________________________________««« END »»»» Edit/update page
 			} else if (checkifexists(model)) {
 				// New page ________
-				if(model.getPage().equals("import") || model.getPage().equals("package")) {
+				if(model.getPage().equals("import") || model.getPage().equals("package") || model.getPage().equals("public")) {
 					Core.setMessageError("Reserved code: "+model.getPage()+". Try another!");					
 					return this.forward("igrp", "page", "index");
 				}
