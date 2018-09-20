0df4ea0b7624358db983359cb6b72afb4361ef15
Marcos Miguel Barros Brito <marcosmb.brito@gmail.com>
2018-09-20 17:43:04 -0100

    v180920 multi invite; ^webreport^; new pics
    NU - multi invite working
    Migration - Root with email nosicode@nosic.v

--- IGRP/src/main/java/nosi/core/igrp/mingrations/MigrationIGRPInitConfig.java
+++ IGRP/src/main/java/nosi/core/igrp/mingrations/MigrationIGRPInitConfig.java
@@ -379,6 +379,7 @@ public class MigrationIGRPInitConfig {
 		User aux = new User();
 		aux.setAuth_key("123456");
 		aux.setCreated_at(12345);
+		aux.setEmail("nosicode@nosi.cv");
 		aux.setPass_hash("Pa$$w0rd");
 		aux.setStatus(1);
 		aux.setUpdated_at(133);
