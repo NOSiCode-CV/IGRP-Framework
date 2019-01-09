937659273c921b01781242363871675c186e588c
Isaias1994GitHub <joaquim199410@gmail.com>
2019-01-09 11:19:15 -0100

    insert page pesquisa bi/cni/pass

---
+++ IGRP/src/migrations/20190108181154_create_action_pesquisa_geral.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.tbl_action(
+	 action, action_descr, iscomponent, package_name, page, page_descr, status, versao, xsl_src, env_fk,tipo)
+	VALUES ('index', 'Pesquisa BI-CNI-passport', 0, 'nosi.webapps.igrp_studio.pages', 'Pesquisa_bi_cni_passport', 'Pesquisa BI-CNI-passport', 1, 2.3,'igrp_studio/pesquisa_bi_cni_passport/Pesquisa_bi_cni_passport.xsl', 3, 0);
+ 
