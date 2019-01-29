a1c1b0a823f4d91beba00f2c3857905a1ba05d82
Isaias1994GitHub <joaquim199410@gmail.com>
2019-01-29 16:16:40 -0100

    Pesquisa Geografia

--- IGRP/src/main/java/nosi/webapps/igrp_studio/pages/pesquisa_geografia/Pesquisa_geografiaController.java
+++ IGRP/src/main/java/nosi/webapps/igrp_studio/pages/pesquisa_geografia/Pesquisa_geografiaController.java
@@ -8,6 +8,11 @@ import nosi.core.webapp.Core;
 import nosi.core.webapp.Response;
 /*----#start-code(packages_import)----*/
 
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import nosi.core.webapp.webservices.rest.ConsumeJson;
 
 /*----#end-code----*/
 		
@@ -17,15 +22,62 @@ public class Pesquisa_geografiaController extends Controller {
 		model.load();
 		Pesquisa_geografiaView view = new Pesquisa_geografiaView();
 		/*----#start-code(index)----*/
-		
-		
+		model.setTreemenu_1(chamarServico("0"));
 		/*----#end-code----*/
 		view.setModel(model);
 		return this.renderView(view);	
 	}
 	
+	public Response actionRemotetreemenu_1(String p_id) throws IOException, IllegalArgumentException, IllegalAccessException{
+		String id = Core.getParam("p_id");
+		String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" 
+		+ " <treemenu_1> "
+				+ "<table>"
+				+ "<value>";
+		List<Pesquisa_geografia.Treemenu_1> lista = chamarServico(id);
+		for(Pesquisa_geografia.Treemenu_1 li : lista) {
+			xml += getXml(li.getTreemenu_1_tmid()+"", li.getTreemenu_1_link_desc(),id);
+		}
+		xml +=  "</value>"
+				+ "</table>"
+				+ "</treemenu_1>";
+		return this.renderView(xml);
+	}
+	
 /*----#start-code(custom_actions)----*/
-
-
+	public String getXml(String id,String desc_menu, String id_par) {
+		String xml =	"<row>" + 
+							"<treemenu_1_link_desc>" + desc_menu + "</treemenu_1_link_desc>" + 
+							"<treemenu_1_tmid>"+ id + "</treemenu_1_tmid>" +
+							"<treemenu_1_parent>"+ id_par +"</treemenu_1_parent>" + 
+							"<treemenu_1_icon/>" + 
+							"<treemenu_1_child>1</treemenu_1_child>" + 
+							"<treemenu_1_active/>" + 
+						"</row>";
+		
+		return xml;
+	}
+	public List<Pesquisa_geografia.Treemenu_1>  chamarServico(String id) throws IOException {
+		String url = "https://stage-pdex.gov.cv:8243/geografia/1.0.0/select_geo_by_id?id="+id;
+		String authorization = "Bearer 18dacc19-f73b-3600-ab37-9fac8eb4f60f";
+		ConsumeJson json_obj = new ConsumeJson();
+		String json  = json_obj.getJsonFromUrl(url, authorization);
+		List<Pesquisa_geografia.Treemenu_1> list_geo = new ArrayList<>();
+		JSONObject obj = new JSONObject(json);
+		JSONObject Entries = obj.getJSONObject("Entries");
+		try {
+			JSONArray Entry = Entries.getJSONArray("Entry");
+			for(int i = 0; i < Entry.length(); i++) {
+				JSONObject local = Entry.getJSONObject(i);
+				Pesquisa_geografia.Treemenu_1 tab_geo = new Pesquisa_geografia.Treemenu_1();
+				tab_geo.setTreemenu_1_tmid(local.getString("id"));
+				tab_geo.setTreemenu_1_link_desc(local.getString("nome"));
+				tab_geo.setTreemenu_1_child("1");
+				list_geo.add(tab_geo);
+			}
+		}catch (Exception e) {
+		}
+		return list_geo;
+	}
 /*----#end-code----*/
 }
--- IGRP/src/main/java/nosi/webapps/igrp_studio/pages/pesquisa_geografia/Pesquisa_geografiaView.java
+++ IGRP/src/main/java/nosi/webapps/igrp_studio/pages/pesquisa_geografia/Pesquisa_geografiaView.java
@@ -12,14 +12,15 @@ import nosi.core.webapp.Report;
 public class Pesquisa_geografiaView extends View {
 
 	public Field sectionheader_1_text;
-	public Field geo_link;
-	public Field geo_tmid;
-	public Field geo_parent;
-	public Field geo_icon;
-	public Field geo_child;
-	public Field geo_active;
+	public Field treemenu_1_link;
+	public Field treemenu_1_link_desc;
+	public Field treemenu_1_tmid;
+	public Field treemenu_1_parent;
+	public Field treemenu_1_icon;
+	public Field treemenu_1_child;
+	public Field treemenu_1_active;
 	public IGRPForm sectionheader_1;
-	public IGRPTable geo;
+	public IGRPTable treemenu_1;
 
 
 	public Pesquisa_geografiaView(){
@@ -28,36 +29,38 @@ public class Pesquisa_geografiaView extends View {
 			
 		sectionheader_1 = new IGRPForm("sectionheader_1","");
 
-		geo = new IGRPTreeMenu("geo","");
+		treemenu_1 = new IGRPTreeMenu("treemenu_1","");
 
 		sectionheader_1_text = new TextField(model,"sectionheader_1_text");
 		sectionheader_1_text.setLabel(gt(""));
 		sectionheader_1_text.setValue(gt("Pesquisa Geografia"));
 		sectionheader_1_text.propertie().add("type","text").add("name","p_sectionheader_1_text").add("maxlength","4000");
 		
-		geo_link = new LinkField(model,"geo_link");
-		geo_link.setLabel(gt("Link"));
-		geo_link.propertie().add("type","link").add("maxlength","4000").add("target","_self").add("desc","true");
+		treemenu_1_link = new LinkField(model,"treemenu_1_link");
+		treemenu_1_link.setLabel(gt("Link"));
+		treemenu_1_link_desc = new LinkField(model,"treemenu_1_link_desc");
+		treemenu_1_link_desc.setLabel(gt("Link"));
+		treemenu_1_link.propertie().add("type","link").add("maxlength","4000").add("target","_self").add("desc","true");
 		
-		geo_tmid = new NumberField(model,"geo_tmid");
-		geo_tmid.setLabel(gt("ID"));
-		geo_tmid.propertie().add("type","number");
+		treemenu_1_tmid = new NumberField(model,"treemenu_1_tmid");
+		treemenu_1_tmid.setLabel(gt("ID"));
+		treemenu_1_tmid.propertie().add("type","number").add("java-type","Integer");
 		
-		geo_parent = new NumberField(model,"geo_parent");
-		geo_parent.setLabel(gt("Parent ID"));
-		geo_parent.propertie().add("type","number");
+		treemenu_1_parent = new NumberField(model,"treemenu_1_parent");
+		treemenu_1_parent.setLabel(gt("Parent ID"));
+		treemenu_1_parent.propertie().add("type","number").add("java-type","Integer");
 		
-		geo_icon = new TextField(model,"geo_icon");
-		geo_icon.setLabel(gt("Icon"));
-		geo_icon.propertie().add("type","text");
+		treemenu_1_icon = new TextField(model,"treemenu_1_icon");
+		treemenu_1_icon.setLabel(gt("Icon"));
+		treemenu_1_icon.propertie().add("type","text");
 		
-		geo_child = new TextField(model,"geo_child");
-		geo_child.setLabel(gt("Has child value(0/X)"));
-		geo_child.propertie().add("type","text");
+		treemenu_1_child = new TextField(model,"treemenu_1_child");
+		treemenu_1_child.setLabel(gt("Has child value(0/X)"));
+		treemenu_1_child.propertie().add("type","text");
 		
-		geo_active = new TextField(model,"geo_active");
-		geo_active.setLabel(gt("Is Active value(true/false)"));
-		geo_active.propertie().add("type","text");
+		treemenu_1_active = new TextField(model,"treemenu_1_active");
+		treemenu_1_active.setLabel(gt("Is Active value(true/false)"));
+		treemenu_1_active.propertie().add("type","text");
 		
 
 
@@ -69,26 +72,29 @@ public class Pesquisa_geografiaView extends View {
 		
 		sectionheader_1.addField(sectionheader_1_text);
 
-		geo.addField(geo_link);
-		geo.addField(geo_tmid);
-		geo.addField(geo_parent);
-		geo.addField(geo_icon);
-		geo.addField(geo_child);
-		geo.addField(geo_active);
+		treemenu_1.addField(treemenu_1_link);
+		treemenu_1.addField(treemenu_1_link_desc);
+		treemenu_1.addField(treemenu_1_tmid);
+		treemenu_1.addField(treemenu_1_parent);
+		treemenu_1.addField(treemenu_1_icon);
+		treemenu_1.addField(treemenu_1_child);
+		treemenu_1.addField(treemenu_1_active);
 
 		this.addToPage(sectionheader_1);
-		this.addToPage(geo);
+		this.addToPage(treemenu_1);
 	}
 		
 	@Override
 	public void setModel(Model model) {
 		
-		geo_link.setValue(model);
-		geo_tmid.setValue(model);
-		geo_parent.setValue(model);
-		geo_icon.setValue(model);
-		geo_child.setValue(model);
-		geo_active.setValue(model);	
+		treemenu_1_link.setValue(model);
+		treemenu_1_link_desc.setValue(model);
+		treemenu_1_tmid.setValue(model);
+		treemenu_1_parent.setValue(model);
+		treemenu_1_icon.setValue(model);
+		treemenu_1_child.setValue(model);
+		treemenu_1_active.setValue(model);	
 
+		treemenu_1.loadModel(((Pesquisa_geografia) model).getTreemenu_1());
 		}
 }
