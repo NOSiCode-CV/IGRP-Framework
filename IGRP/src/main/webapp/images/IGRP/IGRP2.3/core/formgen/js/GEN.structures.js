71aad175f55522a0a0fe8285cf5f4209430e27a7
William Lima <willy.lima89@gmail.com>
2019-03-11 11:10:53 -0100

    Container Max Lenght bug fixed

--- IGRP/src/main/webapp/images/IGRP/IGRP2.3/core/formgen/js/GEN.structures.js
+++ IGRP/src/main/webapp/images/IGRP/IGRP2.3/core/formgen/js/GEN.structures.js
@@ -116,8 +116,11 @@ var GENSTRUCTURES = function(GEN){
 					}
 
 					if(container.xml.type == 'text' || container.xml.type == 'texteditor'){
-						var value   = container.GET.text ? container.GET.text() : '';
-						rtn+='<fields><'+tag+'_text type="text" name="p_'+tag+'_text" persist="true" maxlength="'+container.GET.maxlength()+'"><value><![CDATA['+escapeDoubleQuotes(value)+']]></value></'+tag+'_text></fields>';
+						var value   = container.GET.text ? container.GET.text() : '',
+								
+							mxLength = container.GET.maxlength ? container.GET.maxlength() : '';
+							
+						rtn+='<fields><'+tag+'_text type="text" name="p_'+tag+'_text" persist="true" maxlength="'+mxLength+'"><value><![CDATA['+escapeDoubleQuotes(value)+']]></value></'+tag+'_text></fields>';
 					}
 
 					if(container.xml.type == 'map')
