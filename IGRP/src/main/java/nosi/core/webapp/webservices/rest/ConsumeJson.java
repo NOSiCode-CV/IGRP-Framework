41f4877efa07d3309fe5fddddf9c3aecfbe2acfc
joao.augoncalves <joao.augoncalves@NOSI-DDS-38.gov.cv>
2019-06-12 11:52:43 -0100

    Atualizando ConsumeJson (java 8)

--- IGRP/src/main/java/nosi/core/webapp/webservices/rest/ConsumeJson.java
+++ IGRP/src/main/java/nosi/core/webapp/webservices/rest/ConsumeJson.java
@@ -16,59 +16,54 @@ import nosi.core.webapp.Core;
  */
 public class ConsumeJson {
 	public String getObjectFromJson(String url, String json_data) throws IOException {
-		
-		byte[] postData       = json_data.getBytes(StandardCharsets.UTF_8);
-		int    postDataLength = postData.length;
-		URL    url_request          = new URL( url );
-		HttpURLConnection conn= (HttpURLConnection) url_request.openConnection();           
-		conn.setDoOutput( true );
-		conn.setInstanceFollowRedirects( false );
-		conn.setRequestMethod( "POST" );
-		conn.setRequestProperty( "Content-Type", "application/json"); 
-		conn.setRequestProperty( "charset", "utf-8");
-		conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-		conn.setUseCaches( false );
 
-		try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
-		  wr.write( postData );
+		byte[] postData = json_data.getBytes(StandardCharsets.UTF_8);
+		int postDataLength = postData.length;
+		URL url_request = new URL(url);
+		HttpURLConnection conn = (HttpURLConnection) url_request.openConnection();
+		
+		conn.setDoOutput(true);
+		conn.setInstanceFollowRedirects(false);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		conn.setRequestProperty("charset", "utf-8");
+		conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+		conn.setUseCaches(false);
+
+		try (DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
+			wr.write(postData);
 		}
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-		
-		
+
 		StringBuffer response = new StringBuffer();
-		String inputline;
-		while ((inputline = in.readLine()) != null) {
-		     response.append(inputline);
-		}
-		
+		in.lines().forEach(response::append);
+		in.close();
+		conn.disconnect();
+		System.out.println("response getObjectFromJson: " + response.toString());
 		return response.toString();
 	}
-	
+
 	public String getJsonFromUrl(String url, String authorization) throws IOException {
 		try {
-			URL    url_request          = new URL( url );
-			HttpURLConnection conn= (HttpURLConnection) url_request.openConnection();           
-			conn.setDoOutput( true );
-			conn.setInstanceFollowRedirects( false );
-			conn.setRequestProperty("Authorization", authorization); 
-			conn.setRequestMethod( "POST" );
-			conn.setRequestProperty( "accept", "application/json"); 
-			conn.setRequestProperty( "charset", "utf-8");
-			//conn.setUseCaches( false );
+			URL url_request = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) url_request.openConnection();
+			conn.setDoOutput(true);
+			conn.setInstanceFollowRedirects(false);
+			conn.setRequestProperty("Authorization", authorization);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("accept", "application/json");
+			conn.setRequestProperty("charset", "utf-8");
+			// conn.setUseCaches( false );
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
 			StringBuffer response = new StringBuffer();
-			String inputline;
-			while ((inputline = in.readLine()) != null) {
-			     response.append(inputline);
-			}
+			in.lines().forEach(response::append);
+			in.close();
+			conn.disconnect();
 			return response.toString();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block			
-			Core.setMessageError("Erro: "+e.toString());
-			
+			Core.setMessageError("Erro: " + e.toString());
 			return null;
 		}
 	}
-	
 }
