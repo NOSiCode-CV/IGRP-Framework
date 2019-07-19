335103a2e17c69734541bb02bb0edaa3dd534612
joao.augoncalves <joao.augoncalves@NOSI-DDS-38.gov.cv>
2019-07-19 11:54:34 -0100

    Adding table tbl_temp_file to db_igrp + Helper to do the CRUD

--- IGRP/src/main/java/nosi/core/webapp/Core.java
+++ IGRP/src/main/java/nosi/core/webapp/Core.java
@@ -928,7 +928,11 @@ public final class Core { // Not inherit
 	public static String getLinkFile(int p_id) {
 		return Route.getResolveUrl("igrp", "File", "get-file&p_id=" + p_id);
 	}
-
+	
+	public static String getLinkTempFile(String p_uuid) {
+		return Route.getResolveUrl("igrp", "File", "get-temp-file&p_uuid=" + p_uuid);
+	}
+	
 	/**
 	 * This method you can invoking using Link or Button.
 	 * 
