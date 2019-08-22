5f241ad400e6990069fc3b408cf92d5099a48bfa
emerson <emerson@LAPTOP-UV8UHORO>
2019-08-22 11:13:40 -0100

    Message Validator

--- IGRP/src/main/java/nosi/core/validator/MessageValidator.java
+++ IGRP/src/main/java/nosi/core/validator/MessageValidator.java
@@ -5,23 +5,23 @@ package nosi.core.validator;
  */
 public class MessageValidator {
 
-	public static final String MESSAGE_EMAIL = "{javax.validation.constraints.Email.message}";
+	public static final String MESSAGE_EMAIL = "Please insert a valid Email";
 
 	public static final String MESSAGE_NOT_NULL = "{javax.validation.constraints.NotNull.message}";
 	
-	public static final String MESSAGE_MAX = "{javax.validation.constraints.Max.message}";
+	public static final String MESSAGE_MAX = "It may not exceed the maximum value";
 	
-	public static final String MESSAGE_MIN = "{javax.validation.constraints.Min.message}";
+	public static final String MESSAGE_MIN = "It may not exceed the minimum value";
 	
-	public static final String MESSAGE_NEGATIVE = "{javax.validation.constraints.Negative.message}";
+	public static final String MESSAGE_NEGATIVE = "It must be a Negative value";
 	
-	public static final String MESSAGE_NEGATIVE_OR_ZERO = "{javax.validation.constraints.NegativeOrZero.message}";
+	public static final String MESSAGE_NEGATIVE_OR_ZERO = "It must be a Negative or Zero value";
 	
 	public static final String MESSAGE_SIZE = "{javax.validation.constraints.Size.message}";
 	
-	public static final String MESSAGE_POSITIVE = "{javax.validation.constraints.Positive.message}";
+	public static final String MESSAGE_POSITIVE = "It must be a Positive value";
 	
-	public static final String MESSAGE_POSITIVE_OR_ZERO = "{javax.validation.constraints.PositiveOrZero.message}";
+	public static final String MESSAGE_POSITIVE_OR_ZERO = "It must be a Positive or Zero value";
 
 	public static final String MESSAGE_PATTERN = "{javax.validation.constraints.Pattern.message}";
 
