dce68507b19377727b9de187f81c3ef8275b9ea9
eapereira <emanueld.pereira2@gmail.com>
2019-08-29 11:36:59 -0100

    send class EmailValidator

---
+++ IGRP/src/main/java/nosi/core/validator/EmailValidator.java
@@ -0,0 +1,29 @@
+package nosi.core.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import nosi.core.validator.constraints.Email;
+import nosi.core.webapp.Core;
+
+/**
+ * Emanuel
+ * 29 Aug 2019
+ */
+public class EmailValidator implements ConstraintValidator<Email,String>{
+
+	String regexp;
+	@Override
+	public void initialize(Email constraintAnnotation) {
+		this.regexp = constraintAnnotation.regexp();
+	}
+	
+	@Override
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		if(Core.isNotNull(value)){
+			return Validation.validatePattern(regexp,value);
+		}
+		return true;
+	}
+
+
+}
