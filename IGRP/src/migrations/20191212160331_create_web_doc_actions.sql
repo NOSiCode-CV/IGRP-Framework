4650f4e081a2802a0d2471f658548037d0581da6
Isaias.Nunes <Isaias.Nunes@NOSI-DDS-44.gov.cv>
2019-12-13 11:13:31 -0100

    script insert actions web doc

---
+++ IGRP/src/migrations/20191212160331_create_web_doc_actions.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.tbl_action(
+	 action, action_descr, iscomponent, package_name, page, page_descr, status, versao, xsl_src, env_fk,tipo)
+	VALUES ('index', 'Editar documentacao', 0, 'nosi.webapps.tutorial.pages', 'Editar_documentacao', 'Editar documentacao', 1, 2.3,'tutorial/editar_documentacao/Editar_documentacao.xsl', 2, 0);
+	
+	INSERT INTO public.tbl_action(
+	 action, action_descr, iscomponent, package_name, page, page_descr, status, versao, xsl_src, env_fk,tipo)
+	VALUES ('index', 'Parametrizacao tipo', 0, 'nosi.webapps.tutorial.pages', 'Parametrizacao_tipo', 'Parametrizacao tipo', 1, 2.3,'tutorial/parametrizacao_tipo/Parametrizacao_tipo.xsl', 2, 0);
+	
+	INSERT INTO public.tbl_action(
+	 action, action_descr, iscomponent, package_name, page, page_descr, status, versao, xsl_src, env_fk,tipo)
+	VALUES ('index', 'Listar documentos', 0, 'nosi.webapps.tutorial.pages', 'Listar_documentos', 'Listar documentos', 1, 2.3,'tutorial/listar_documentos/Listar_documentos.xsl', 2, 0);
+	
+	INSERT INTO public.tbl_action(
+	 action, action_descr, iscomponent, package_name, page, page_descr, status, versao, xsl_src, env_fk,tipo)
+	VALUES ('index', 'Inserir documento', 0, 'nosi.webapps.tutorial.pages', 'Inserir_documento', 'Inserir documento', 1, 2.3,'tutorial/inserir_documento/Inserir_documento.xsl', 2, 0);
+	
