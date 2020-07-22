4ef5fa8029138cd0a392d9ab8d0d6f0cd3d7ad02
Iekiny Marcel <Iekiny Marcel@NOSI-DDS-41.gov.cv>
2020-07-22 20:48:11 -0100

    Fix bug validate input Docs processo

--- IGRP/src/main/java/nosi/core/webapp/bpmn/ValidateInputDocument.java
+++ IGRP/src/main/java/nosi/core/webapp/bpmn/ValidateInputDocument.java
@@ -28,10 +28,9 @@ public class ValidateInputDocument {
 	 * @throws IOException
 	 * @throws ServletException
 	 */
-	public static boolean validateRequiredDocument(InterfaceBPMNTask bpm,List<Part> parts_, RuntimeTask runtimeTask) throws IOException, ServletException {
+	public static boolean validateRequiredDocument(InterfaceBPMNTask bpm,List<Part> parts_, RuntimeTask runtimeTask, List<String> errors) throws IOException, ServletException {
 		List<TipoDocumentoEtapa> list = BPMNHelper.getInputDocumentType(runtimeTask.getTask().getTenantId(),runtimeTask.getTask().getProcessDefinitionKey(),runtimeTask.getTask().getTaskDefinitionKey());
 		Map<Integer,TipoDocumentoEtapa> listMap = list.stream().collect(Collectors.toMap(TipoDocumentoEtapa::getId,tp->tp));
-		boolean result = true;
 		Object[] p_ids = Core.getParamArray("p_formlist_documento_id_tp_doc_fk");	
 		if(listMap!=null && p_ids!=null) {
 			p_ids = Arrays.asList(p_ids).stream().filter(value->Core.isNotNull(value)).toArray();
@@ -39,14 +38,12 @@ public class ValidateInputDocument {
 				List<Part> parts = parts_.stream().filter(p->p.getName().equalsIgnoreCase("p_formlist_documento_task_documento_fk")).collect(Collectors.toList());
 				for(int i=0;i<p_ids.length;i++) {		
 					TipoDocumentoEtapa tp = listMap.get(Core.toInt(p_ids[i].toString())); 	
-					if(tp!=null && tp.getRequired()==1 && !validateRequired(tp,parts,i)){
-						result  = false;
-						Core.setMessageError("Deve fazer upload do ficheiro: "+tp.getTipoDocumento().getNome());
-					}
+					if(tp!=null && tp.getRequired()==1 && !validateRequired(tp,parts,i)) 
+						errors.add("Deve fazer upload do ficheiro: "+tp.getTipoDocumento().getNome()); 
 				}
 			}
 		}
-		return result;
+		return errors.isEmpty();
 	}
 
 	private static boolean validateRequired(TipoDocumentoEtapa tp, List<Part> parts, int index) {
