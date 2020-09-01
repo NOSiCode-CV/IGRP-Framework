27f471dba8bbeb461ccd82aeac67129b3b9a574a
Isaias.Nunes <Isaias.Nunes@NOSI-DDS-44.gov.cv>
2020-09-01 10:51:34 -0100

    Revert "Fix bug: Erro ForeinKey do Datasource ao importar Report"
    This reverts commit 6916793f5bf1fd4bd5c46e3430c4ee74723b6f46.

--- IGRP/src/main/java/nosi/core/webapp/import_export_v2/imports/report/ReportImport.java
+++ IGRP/src/main/java/nosi/core/webapp/import_export_v2/imports/report/ReportImport.java
@@ -2,6 +2,7 @@ package nosi.core.webapp.import_export_v2.imports.report;
 
 import java.util.List;
 import com.google.gson.reflect.TypeToken;
+
 import nosi.core.config.ConfigDBIGRP;
 import nosi.core.webapp.Core;
 import nosi.core.webapp.import_export_v2.common.serializable.report.CLobSerializable;
