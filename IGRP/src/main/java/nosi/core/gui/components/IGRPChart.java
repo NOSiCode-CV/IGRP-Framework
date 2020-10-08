95708acaddf153829345bfe12fe90a3740395565
Marcos Miguel Barros Brito <marcosmb.brito@gmail.com>
2020-10-08 12:18:31 -0100

    HOTFIX chart 2D by Class not in the correct order; Report comment
    -Chart 2D by Class was not displaying in the correct order of added
    -Report was added the comment new nosi.core.webapp.report.Report() to
    prevent the need to import this class Report()

--- IGRP/src/main/java/nosi/core/gui/components/IGRPChart.java
+++ IGRP/src/main/java/nosi/core/gui/components/IGRPChart.java
@@ -44,11 +44,11 @@ import nosi.core.webapp.Core;
 import nosi.core.webapp.databse.helpers.BaseQueryInterface;
 import nosi.core.webapp.databse.helpers.DatabaseMetadaHelper;
 import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
+
 import java.util.Set;
 import javax.persistence.Tuple;
 
@@ -150,8 +150,8 @@ public class IGRPChart extends IGRPComponent{
 			}
 		}
 		this.generateLabels(labels);
-		Map<String, Object> valuesXY = new HashMap<>();
-		Map<String,Map<String,Object>> valuesXYZ = new HashMap<>();		
+		LinkedHashMap<String, Object> valuesXY = new LinkedHashMap<>();
+		LinkedHashMap<String,LinkedHashMap<String,Object>> valuesXYZ = new LinkedHashMap<>();		
 		for(Object o:this.data) {
 			if(o instanceof IGRPChart2D) {
 				IGRPChart2D chart2d = (IGRPChart2D) o;
@@ -159,7 +159,7 @@ public class IGRPChart extends IGRPComponent{
 			}
 			else if(o instanceof IGRPChart3D) {
 				IGRPChart3D chart3d = (IGRPChart3D) o;
-				Map<String, Object> value = new HashMap<>();
+				LinkedHashMap<String, Object> value = new LinkedHashMap<>();
 				value.put(chart3d.getEixoY(), ""+chart3d.getEixoZ());
 				valuesXYZ.put(chart3d.getEixoX(), value );
 			}
@@ -269,7 +269,7 @@ public class IGRPChart extends IGRPComponent{
 		this.xml.endElement();
 	}
 	
-	private void generateRowsValueXY(Map<String,Object> valuesXY) {
+	private void generateRowsValueXY(LinkedHashMap<String,Object> valuesXY) {
 		this.xml.startElement("value");
 		this.xml.startElement("row");
 		this.xml.setElement("col"," ");
--- IGRP/src/main/java/nosi/webapps/igrp_studio/pages/webreport/WebReportController.java
+++ IGRP/src/main/java/nosi/webapps/igrp_studio/pages/webreport/WebReportController.java
@@ -78,7 +78,7 @@ public class WebReportController extends Controller {
 						if(param.getParameters() != null) 
 							for(RepTemplateSourceParam p:param.getParameters()) 
 								params += ".addParam(\""+p.getParameter().toLowerCase()+"\",\"?\")";
-				String link = "Core.getLinkReport(\""+r.getCode()+"\")"+params+"; //or Core.getLinkReport(\""+r.getCode()+"\",new Report()"+params+");";
+				String link = "Core.getLinkReport(\""+r.getCode()+"\")"+params+"; //or Core.getLinkReport(\""+r.getCode()+"\",new nosi.core.webapp.report.Report()"+params+");";
 				t1.setDescricao(link);
 				t1.setLink("igrp_studio", "web-report", "load-template&id="+r.getId());
 				t1.setLink_desc(r.getCode());
