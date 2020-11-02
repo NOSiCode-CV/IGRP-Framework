4f481264f4e48de1fe034112446586c131fafb1e
edsonch4 <edson.tavares@ideiacv.com>
2020-11-02 09:44:21 -0100

    bug fixed and total table improvements

--- IGRP/src/main/webapp/images/IGRP/IGRP2.3/core/formgen/types/containers/table/js/table.js
+++ IGRP/src/main/webapp/images/IGRP/IGRP2.3/core/formgen/types/containers/table/js/table.js
@@ -688,13 +688,15 @@ var GENTABLE = function(name,params){
 			
 			if($.inArray(f.type,container.reject) === -1 && f.GET.tag() != container.GET.tag()+'_filter' && f.type != 'hidden'){
 				
-				var fValue = container.GET.path()+"/fields/"+f.GET.tag(),
+				var fValue = container.GET.path()+"/table/value/row[@total='yes']/"+f.GET.tag(),
 				
 					align  =  f.GET.align ? f.GET.align() : '';
 			
-				rtn+='<xsl:if test="//'+fValue+'"><td align="'+align+'" td-name="'+f.GET.tag()+'">';
+				rtn+='<xsl:if test="//'+container.GET.path()+"/fields/"+f.GET.tag()+'">';
 				
-				rtn+='<xsl:value-of select="'+fValue+'"/>';
+					rtn+='<td align="'+align+'" item-name="'+f.GET.tag()+'" td-name="'+f.GET.tag()+'">';
+				
+						rtn+='<xsl:value-of select="'+fValue+'"/>';
 				
 				rtn+='</td></xsl:if>';
 			}
