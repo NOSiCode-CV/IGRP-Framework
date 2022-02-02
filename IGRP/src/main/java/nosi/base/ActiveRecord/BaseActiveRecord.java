4caaefc71ab80f2ad94292ffa52bad3330921697
Marcos M. B. Brito <marcos.m.brito@nosi.cv>
2022-02-02 08:49:24 -0100

    To make BDD work, this is a forcing bd connection to change for mock use

--- IGRP/src/main/java/nosi/base/ActiveRecord/BaseActiveRecord.java
+++ IGRP/src/main/java/nosi/base/ActiveRecord/BaseActiveRecord.java
@@ -1714,10 +1714,15 @@ public abstract class BaseActiveRecord<T> implements ActiveRecordIterface<T>, Se
 	}
 	
 	public String defaultConnection(String dad) {
+		//To make BDD work, this is a forcing bd connection to change for mock use
+		final String connectionTestName = Core.getParam("igrp.test.bdd",false);
+		if (Core.isNotNull(connectionTestName)) {
+			return connectionTestName;
+		}
 		String result = "";
-		Application app = new Application().setKeepConnection(this.keepConnection).find().andWhere("dad", "=", dad).one();
+		Application app = new Application().setKeepConnection(true).find().andWhere("dad", "=", dad).one();
 		if (app != null) {
-			Config_env config_env = new Config_env().setKeepConnection(this.keepConnection).find().andWhere("isdefault", "=", new Short((short) 1))
+			Config_env config_env = new Config_env().setKeepConnection(true).find().andWhere("isdefault", "=", (short) 1)
 					.andWhere("application", "=", app.getId()).one();
 			if (config_env != null)
 				result = config_env.getName();
