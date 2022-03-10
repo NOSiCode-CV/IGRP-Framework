bc81a05ccaaf9945ae3016b9bda95719c5835592
Isaias1994GitHub <joaquim199410@gmail.com>
2022-03-10 11:35:14 -0500

    param center map [by Carlos]

--- IGRP/src/main/java/nosi/core/gui/geographic/MapViewer.java
+++ IGRP/src/main/java/nosi/core/gui/geographic/MapViewer.java
@@ -4,67 +4,78 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 /**
- * CarlosA.Graca
- * 29/07/2020
+ * CarlosA.Graca 29/07/2020
  */
 public class MapViewer {
-	
+
 	private int zoom;
-	
+
 	private double[] center;
-	
+
 	private boolean fullscreen;
-  
-    private JsonObject json = new JsonObject();
-	
+
+	private JsonObject json = new JsonObject();
+
 	private JsonArray baseMaps = new JsonArray();
-	
+
 	private JsonArray layers = new JsonArray();
-	
+
 	private JsonArray widgets = new JsonArray();
-  
-  	public void add(String obj, boolean value) {
-    	
-    	this.json.addProperty(obj, value);
-    	
-    }
-	
+
+	public void add(String obj, boolean value) {
+
+		this.json.addProperty(obj, value);
+
+	}
+
+	public void add(String obj, String value) {
+
+		this.json.addProperty(obj, value);
+
+	}
+
+	public void add(String obj, JsonArray value) {
+
+		this.json.add(obj, value);
+
+	}
+
 	public JsonArray getWidgets() {
 		return widgets;
 	}
-	
+
 	public void setWidgets(JsonArray widgets) {
 		this.widgets = widgets;
 	}
-	
-    public void addBaseMap(JsonObject basemap) {
-    	
-    	this.baseMaps.add(basemap);
-    	
-    }
-    
-    public void addLayer(JsonObject layer) {
-    	
-    	this.layers.add(layer);
-    	
-    }
-    
-    public void addWidget(JsonObject widget) {
-    	
-    	this.widgets.add(widget);
-    	
-    }
-    
-    public String toString (){
-    	    	
-    	this.json.add("baseMaps", this.baseMaps);
-    	
-    	this.json.add("groupLayers", this.layers);
-    	    			
-    	this.json.add("widgets", this.widgets);
-    	    	
-    	return this.json.toString();
-    	
-    }
+
+	public void addBaseMap(JsonObject basemap) {
+
+		this.baseMaps.add(basemap);
+
+	}
+
+	public void addLayer(JsonObject layer) {
+
+		this.layers.add(layer);
+
+	}
+
+	public void addWidget(JsonObject widget) {
+
+		this.widgets.add(widget);
+
+	}
+
+	public String toString() {
+
+		this.json.add("baseMaps", this.baseMaps);
+
+		this.json.add("groupLayers", this.layers);
+
+		this.json.add("widgets", this.widgets);
+
+		return this.json.toString();
+
+	}
 
 }
