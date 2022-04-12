7999e143b10af2178389642d279d5560591bdca9
william.fonseca <william.fonseca@nosi.cv>
2022-04-12 10:26:45 -0100

    Sql Tools: Remove need of two database access for retrieving query data and column names since the query data already have column names; Reduce database queries executed in Transaction.getPermission method; QueryHelper: Show error messages when executing queries in SqlTools by showing error message if showError is set to true; Other java language migration aids: unnecessary boxing, use of string builder instead string concatenation that can prevent memory leaks for example, etc.

--- IGRP/src/main/java/nosi/core/gui/fields/TextField.java
+++ IGRP/src/main/java/nosi/core/gui/fields/TextField.java
@@ -1,27 +1,28 @@
 package nosi.core.gui.fields;
+
 /**
  * @author: Emanuel Pereira
- * 
+ * <p>
  * Apr 13, 2017
- *
+ * <p>
  * Description: class to configure text field
  */
 public class TextField extends AbstractField {
 
-	public TextField(Object model,String name){
-		super();
-		this.propertie.put("type","text");
-		this.setName(name);
-		this.setTagName(name);
-		this.propertie.put("name","p_"+name);
-		this.propertie.put("right", new Boolean(false));
-		this.propertie.put("placeholder", "");
-		this.propertie.put("disabled", new Boolean(false));
-		this.propertie.put("readonly", new Boolean(false));
-		this.propertie.put("change", new Boolean(false));
-		this.propertie.put("required", new Boolean(false));
-		this.propertie.put("maxlength", new Integer(30));
-		this.setValue(model);
-	}
+    public TextField(Object model, String name) {
+        super();
+        this.propertie.put("type", "text");
+        this.setName(name);
+        this.setTagName(name);
+        this.propertie.put("name", "p_" + name);
+        this.propertie.put("right", Boolean.FALSE);
+        this.propertie.put("placeholder", "");
+        this.propertie.put("disabled", Boolean.FALSE);
+        this.propertie.put("readonly", Boolean.FALSE);
+        this.propertie.put("change", Boolean.FALSE);
+        this.propertie.put("required", Boolean.FALSE);
+        this.propertie.put("maxlength", 30);
+        this.setValue(model);
+    }
 
 }
