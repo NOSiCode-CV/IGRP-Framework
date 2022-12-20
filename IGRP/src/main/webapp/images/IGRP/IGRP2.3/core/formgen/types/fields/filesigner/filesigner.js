04149d7158ae024df34c041aa155d15588639cfd
Edson Tavares <edson.tavares@nosi.cv>
2022-12-20 09:52:34 -0100

    bug fixed

--- IGRP/src/main/webapp/images/IGRP/IGRP2.3/core/formgen/types/fields/filesigner/filesigner.js
+++ IGRP/src/main/webapp/images/IGRP/IGRP2.3/core/formgen/types/fields/filesigner/filesigner.js
@@ -22,8 +22,8 @@ var FILESIGNER = function(type,params){
 			value:false,
 			editable:false,
 			xslValue: function(){
-				console.log(field)
-				const pathValue = typeParent === 'formlist' ? field.GET.tag() : `row/content/${field.parent.GET.tag()}/fields/${field.GET.tag()}/value`;
+				
+				const pathValue = typeParent === 'formlist' ? field.GET.tag() : `rows/content/${field.parent.GET.tag()}/fields/${field.GET.tag()}/value`;
 
 				return `<xsl:if test=" ${pathValue} != ''">
 					<xsl:attribute name="href">
