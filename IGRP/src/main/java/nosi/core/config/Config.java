3bd31c93b6385155f09e038b07c3043b5ba75881
Francisco Horta <francisco.horta@nosi.cv>
2022-12-20 15:01:47 -0100

    Core functions added.

--- IGRP/src/main/java/nosi/core/config/Config.java
+++ IGRP/src/main/java/nosi/core/config/Config.java
@@ -24,7 +24,7 @@ public class Config {
     private static final String SEPARATOR_FOR_HTTP = "/";
     private static final String SEPARATOR_FOR_FILESYSTEM = File.separator;
     public static final String BASE_PATH_CONFIGURATION = "config";
-    public static final String VERSION = "1.3.0.220702";
+    public static final String VERSION = "1.5.0.221212";
 
     public String getLinkXSLLogin() {
         String linkXslLogin = "images/IGRP/IGRP2.3/xsl/IGRP-login.xsl";
