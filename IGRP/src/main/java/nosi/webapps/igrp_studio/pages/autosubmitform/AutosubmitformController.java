7a9f3db7c54b6a59736c2efe1ff21c913272e123
IMarcelF <iekinyfernandes@gmail.com>
2023-01-19 13:42:34 -0100

    Autosubmitform (Automatic Form Submit)

---
+++ IGRP/src/main/java/nosi/webapps/igrp_studio/pages/autosubmitform/AutosubmitformController.java
@@ -0,0 +1,34 @@
+package nosi.webapps.igrp_studio.pages.autosubmitform;
+
+import nosi.core.webapp.Controller;//
+import nosi.core.webapp.databse.helpers.ResultSet;//
+import nosi.core.webapp.databse.helpers.QueryInterface;//
+import java.io.IOException;//
+import nosi.core.webapp.Core;//
+import nosi.core.webapp.Response;//
+/* Start-Code-Block (import) */
+/* End-Code-Block */
+/*----#start-code(packages_import)----*/
+
+
+/*----#end-code----*/
+		
+public class AutosubmitformController extends Controller {
+	public Response actionIndex() throws IOException, IllegalArgumentException, IllegalAccessException{
+		Autosubmitform model = new Autosubmitform();
+		model.load();
+		AutosubmitformView view = new AutosubmitformView();
+		/* Start-Code-Block (index) *//* End-Code-Block (index) */
+		/*----#start-code(index)----*/
+		
+		
+		/*----#end-code----*/
+		view.setModel(model);
+		return this.renderView(view);	
+	}
+	/* Start-Code-Block (custom-actions)  *//* End-Code-Block  */
+/*----#start-code(custom_actions)----*/
+
+
+/*----#end-code----*/
+}
