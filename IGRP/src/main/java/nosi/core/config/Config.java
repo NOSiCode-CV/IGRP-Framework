fbdc603f5ff14eccee6e44a2f49607f8a51de519
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2023-05-12 23:50:26 -0100

    @tag v.1.7.1.230512

--- IGRP/src/main/java/nosi/core/config/Config.java
+++ IGRP/src/main/java/nosi/core/config/Config.java
@@ -24,7 +24,7 @@ public class Config {
     private static final String SEPARATOR_FOR_HTTP = "/";
     private static final String SEPARATOR_FOR_FILESYSTEM = File.separator;
     public static final String BASE_PATH_CONFIGURATION = "config";
-    public static final String VERSION = "1.7.1.230502";
+    public static final String VERSION = "1.7.1.230512";
 
     public String getLinkXSLLogin() {
         String linkXslLogin = "images/IGRP/IGRP2.3/xsl/IGRP-login.xsl";
