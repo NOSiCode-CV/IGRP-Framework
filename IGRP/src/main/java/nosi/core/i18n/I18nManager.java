6cfea8c239ec9b2383677ef2a4de47f757c6a7ac
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2023-10-16 15:40:32 +0100

    Fix: Flag of language requested

--- IGRP/src/main/java/nosi/core/i18n/I18nManager.java
+++ IGRP/src/main/java/nosi/core/i18n/I18nManager.java
@@ -20,7 +20,7 @@ public final class I18nManager implements Component{
 	public static final String DEFAULT_CORE_BUNDLE_NAME = "nosi.core.i18n.igrp";
 	public static final String DEFAULT_APP_BUNDLE_NAME = "nosi.webapps.dad.i18n.Resources"; 
 	
-	public static final List<String> SKIP_DADS = new ArrayList<String>(Arrays.asList("igrp", "igrp_studio", "tutorial")); 
+	protected static final List<String> SKIP_DADS = new ArrayList<>(Arrays.asList("igrp", "igrp_studio", "tutorial")); 
 	
 	public static final int COOKIE_EXPIRE = 60*60*24*30; // 1 months 
 	
@@ -31,7 +31,7 @@ public final class I18nManager implements Component{
 	
 	private void newConfiguration() {
 		String v = "";
-		Cookie cookies[] = request.getCookies();
+		Cookie[] cookies = request.getCookies();
 		if(cookies != null)
 			for(Cookie cookie : cookies)
 				if(cookie!=null && cookie.getName()!=null && cookie.getName().equals("igrp_lang")) {
@@ -40,7 +40,7 @@ public final class I18nManager implements Component{
 				}
 		String currentApp = Core.getCurrentDad(); 
 		String defaultAppBundleName = DEFAULT_APP_BUNDLE_NAME.replace("dad", currentApp); 
-		String []aux = null;  
+		String[] aux = null;  
 		if(v != null && !v.isEmpty() && (aux = v.split("_")).length == 2) {// cookie ok 
 			this.coreLanguage = I18nFactory.createI18n(DEFAULT_CORE_BUNDLE_NAME, new Locale(aux[0], aux[1])); 
 			this.appLanguage = !SKIP_DADS.contains(currentApp) ? I18nFactory.createI18n(defaultAppBundleName, new Locale(aux[0], aux[1])) : this.coreLanguage; 
