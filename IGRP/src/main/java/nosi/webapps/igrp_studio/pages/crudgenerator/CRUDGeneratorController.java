f95042e681c8412f4f1c0688688d0ad21507855e
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2023-12-12 14:33:06 -0100

    Check with 100 maxlenght

--- IGRP/src/main/java/nosi/webapps/igrp_studio/pages/crudgenerator/CRUDGeneratorController.java
+++ IGRP/src/main/java/nosi/webapps/igrp_studio/pages/crudgenerator/CRUDGeneratorController.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import nosi.core.config.Config;
 import nosi.core.gui.page.Page;
 import nosi.core.webapp.compiler.helpers.Compiler;
-import nosi.core.webapp.databse.helpers.Connection;
 import nosi.core.webapp.databse.helpers.DatabaseMetadaHelper;
 import nosi.core.webapp.helpers.CheckBoxHelper;
 import nosi.core.webapp.helpers.FileHelper;
@@ -62,7 +61,8 @@ public class CRUDGeneratorController extends Controller {
 			view.btn_add_datasource.setLink("igrp", "ConfigDatabase", "index");
 			view.aplicacao.setValue(new Application().getListApps());
 			view.table_type.setValue(DatabaseMetadaHelper.getTableTypeOptions());
-
+			view.check_table.propertie().add("maxlength", 100);
+			view.check_table_check.propertie().add("maxlength", 100);
 			view.documento.setValue("https://docs.igrp.cv/IGRP/app/webapps?r=tutorial/Listar_documentos/index&dad=tutorial&target=_blank&isPublic=1&lang=pt_PT&p_type=crud");
 			view.forum.setValue("https://gitter.im/igrpweb/crud_dao_generator?utm_source=share-link&utm_medium=link&utm_campaign=share-link");
 			
