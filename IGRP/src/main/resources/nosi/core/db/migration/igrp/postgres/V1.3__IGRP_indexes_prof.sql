7d8e7b6e64e449d44eafb1888bc08b5728f9deaa
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2023-12-22 17:26:04 -0100

    indexes on hold

--- IGRP/src/main/resources/nosi/core/db/migration/igrp/postgres/V1.3__IGRP_indexes_prof.sql
+++ IGRP/src/main/resources/nosi/core/db/migration/igrp/postgres/V1.3__IGRP_indexes_prof.sql
@@ -1,1 +1,2 @@
-CREATE INDEX IF NOT EXISTS profTypeUs on tbl_profile (type_fk, type,user_fk)
+CREATE INDEX IF NOT EXISTS profmp on tbl_profile (type_fk, type,user_fk)
+CREATE INDEX IF NOT EXISTS menu_ae on tbl_menu (action_fk, env_fk)
