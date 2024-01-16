b93a3d04ed9bf7f8e271d8610e006b567de49e63
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2024-01-16 01:02:27 +0100

    v.1..240116

--- IGRP/src/main/java/nosi/core/config/Config.java
+++ IGRP/src/main/java/nosi/core/config/Config.java
@@ -24,7 +24,7 @@ public class Config {
     private static final String SEPARATOR_FOR_HTTP = "/";
     private static final String SEPARATOR_FOR_FILESYSTEM = File.separator;
     public static final String BASE_PATH_CONFIGURATION = "config";
-    public static final String VERSION = "1.8.0.240109";
+    public static final String VERSION = "1.8.0.240116";
     public static final String DEFAULT_V_PAGE = "2.3";
     private static final Properties configs = new Properties();
 
