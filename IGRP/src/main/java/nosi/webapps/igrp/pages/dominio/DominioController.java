ce37f7f0c939b1b35857ef22ad310f79f071edaf
william.fonseca <william.fonseca@nosi.cv>
2024-03-23 14:13:26 -0100

    Show warning when user inserts more than one domain with same description; Give error when user try to insert more than one domain with same key; Maintain data in screen for user also

--- IGRP/src/main/java/nosi/webapps/igrp/pages/dominio/DominioController.java
+++ IGRP/src/main/java/nosi/webapps/igrp/pages/dominio/DominioController.java
@@ -46,11 +46,10 @@ public class DominioController extends Controller {
 	     	mapDom.put(null,gt("++ Adicione um domínio ++"));
 			view.lst_dominio.setValue(mapDom);
 		}else
-			view.lst_dominio.setQuery(domainQuery, gt("-- Selecione ou adicione um domínio ++"));	
+			view.lst_dominio.setQuery(domainQuery, gt("-- Selecione ou adicione um domínio ++"));
 
-		if(Core.isNotNull(model.getLst_dominio())) {  
-			model.setFormlist_1(DomainHeper.getDomainItemQuery(model.getLst_dominio(),app));
-    	}
+		if (Core.isNotNull(model.getLst_dominio()) && Core.isNull(Core.getParam("error_saving_item")))
+			model.setFormlist_1(DomainHeper.getDomainItemQuery(model.getLst_dominio(), app));
       
 		if(Core.isNotNullOrZero(app)) {
 			 view.btn_gravar_domain.addParameter("p_aplicacao",app);
@@ -59,12 +58,12 @@ public class DominioController extends Controller {
         view.btn_gravar_domain.setVisible(Core.isNull(model.getLst_dominio()));           
      	view.btn_guardar_item_domain.setVisible(Core.isNotNull(model.getLst_dominio()));
      	//cod pa table de lista de dominio
-     	if(Core.isNotNull(model.getAplicacao())) {
-     		view.table_1.setVisible(true);
-     		model.loadTable_1(Core.query(this.configApp.getBaseConnection(), "SELECT DISTINCT dominio as id_dom, dominio  as dominio FROM tbl_domain WHERE env_fk=:env_fk").addInt("env_fk", model.getAplicacao()));
-     	}else {
-     		view.table_1.setVisible(false);
-     	}
+		if (Core.isNotNull(model.getAplicacao())) {
+			view.table_1.setVisible(true);
+			model.loadTable_1(Core.query(this.configApp.getBaseConnection(), "SELECT DISTINCT dominio as id_dom, dominio  as dominio FROM tbl_domain WHERE env_fk=:env_fk").addInt("env_fk", model.getAplicacao()));
+		} else {
+			view.table_1.setVisible(false);
+		}
      	
 		/*----#end-code----*/
 		view.setModel(model);
@@ -83,16 +82,19 @@ public class DominioController extends Controller {
 		  Use model.validate() to validate your model
 		  ----#gen-example */
 		/*----#start-code(guardar_item_domain)----*/
-		if(DomainHeper.saveItemDomain(model)) {
+
+		if (DomainHeper.saveItemDomain(model)) {
 			Core.setMessageSuccess();
 			this.addQueryString("p_aplicacao", model.getAplicacao());
 			this.addQueryString("p_lst_dominio", model.getLst_dominio());
-          	this.addQueryString("target", Core.getParam("target"));
-			return this.redirect("igrp","Dominio","index", this.queryString());
-		}else {
+			this.addQueryString("target", Core.getParam("target"));
+			return this.redirect("igrp", model.getClass().getSimpleName(), "index", this.queryString());
+		} else {
 			Core.setMessageError();
-			return this.forward("igrp","Dominio","index", this.queryString());
+			this.addQueryString("error_saving_item", true);
+			return this.forward("igrp", model.getClass().getSimpleName(), "index", this.queryString());
 		}
+
 		/*----#end-code----*/
 			
 	}
