347e03f187dd7845d89915fb691c148bfd030887
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2024-07-25 12:26:04 -0100

    From commit #2242b25a improvs

--- IGRP/src/main/webapp/images/IGRP/IGRP2.4/core/formgen/types/containers/project_task/js/project_task.js
+++ IGRP/src/main/webapp/images/IGRP/IGRP2.4/core/formgen/types/containers/project_task/js/project_task.js
@@ -1,46 +1,48 @@
 (function(){
     var GEN = VARS.getGen();
     GEN.declareContainer({
-
-        name:'project_task',
+      name: "project_task",
 
         container:function(name,params){
             var component = this;
 
-            // const BackgroundProperties = GEN.getGlobalProperty(
-            //     "button-appearance-properties"
-            //   );
+        const BackgroundProperties = GEN.getGlobalProperty(
+            "button-appearance-properties"
+          );
 
-            CONTAINER.call(this,name,params)
+        CONTAINER.call(this, name, params);
 
-            component.xml.structure = 'form';
+        component.xml.structure = "form";
 
             component.includes = {
-
-                js :[ { path:'/components/project_task/project_task.js' }],
-                css:[ { path:'/components/project_task/project_task.css'}]
-        
+          css: [{ path: "/components/project_task/project_task.css" }],
             };
         
+        component.ready = function () {
+          BackgroundProperties(component, {
+            name: "color",
+            showbtnStyle: false,
+            isField: true,
+            valuePersist: true,
+          });
 
             component.unsetProprieties(["hasTitle"]);
 
             component.propertiesOptions.title = {
-                name         : 'title',
-                label        : 'Title',
+            name: "title",
+            label: "Title",
                 isField      : true,
-                valuePersist : false
-    
-            }
+            valuePersist: false,
+          };
 
             component.ready = function(){
                 component.setProperty({
-                    name    : 'subhead',
-                    label   : 'Subtítulo',
-                    value   : 'Project Managment',
-                    isField   : 'true',
-                    valuePersist   :false
-                })
+              name: "subhead",
+              label: "Subtítulo",
+              value: "Project Managment",
+              isField: "true",
+              valuePersist: false,
+            });
 
                 // BackgroundProperties(component,{
                 //     name: 'color',
@@ -48,12 +50,8 @@
                 //     isField   : true,
                 //     valuePersist : true,
                 // });
-
-            }
-
-        }
-        
-
-    })
-
-})()
+          };
+        };
+      },
+    });
+  })();
