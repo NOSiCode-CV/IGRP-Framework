1208076ac587a29dabceec1fb5c4d54b1d8150da
NOSI / DUA - Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2024-08-08 00:53:03 +0000

    Update DominioController.java

--- IGRP/src/main/java/nosi/webapps/igrp/pages/dominio/DominioController.java
+++ IGRP/src/main/java/nosi/webapps/igrp/pages/dominio/DominioController.java
@@ -52,7 +52,11 @@ public class DominioController extends Controller {
 
 		final BaseQueryInterface domainQuery = DomainHeper.getDomainQuery(app);
 		view.lst_dominio.setQuery(domainQuery, gt("-- Selecione/Adicione um domínio --"));
-
+		if(view.lst_dominio.getListOptions()==null){
+			LinkedHashMap<Object,Object> mapDom=new LinkedHashMap<>();
+			mapDom.put(null,gt("++ Adicione um domínio ++"));
+			view.lst_dominio.setValue(mapDom);
+		}
 		if (Core.isNotNull(model.getLst_dominio()) && Core.isNull(Core.getParam("error_saving_item")))
 			model.setFormlist_1(DomainHeper.getDomainItemQuery(model.getLst_dominio(), app));
 
