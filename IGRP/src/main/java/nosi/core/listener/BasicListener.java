9d9f360c2eff8180c7a222419b7969fa38f9d1e3
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2024-08-22 21:41:04 -0100

    banner.txt Added to boot.

--- IGRP/src/main/java/nosi/core/listener/BasicListener.java
+++ IGRP/src/main/java/nosi/core/listener/BasicListener.java
@@ -5,6 +5,15 @@ import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
 import nosi.base.ActiveRecord.HibernateUtils;
+import nosi.core.config.Config;
+import nosi.core.db.migration.api.MigrationIGRPInitConfig;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 /**
  * Marcel Iekiny
  * Nov 23, 2017
@@ -12,16 +21,34 @@ import nosi.base.ActiveRecord.HibernateUtils;
 @WebListener
 public class BasicListener implements ServletContextListener { 
 	
+   @Override
+   public void contextInitialized(ServletContextEvent arg0) {
+      System.out.println("||| iGRP: BasicListener will start migration check! |||");
+      MigrationIGRPInitConfig.start();
+      printBanner();
+   }
+
+
 
     @Override
     public void contextDestroyed(ServletContextEvent arg0)  {
     	HibernateUtils.closeAllConnection();
     	HibernateUtils.unregisterAllDrivers();
+      System.out.println("||| iGRP: BasicListener will see you soon! Txau |||");
+   }
+
+   private void printBanner() {
+      try (InputStream inputStream = BasicListener.class.getResourceAsStream("/banner.txt");
+           BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+
+         String line;
+         while ((line = reader.readLine()) != null) {
+            System.out.println(line.replace("#version", Config.VERSION));
     }
     
-	@Override
-	public void contextInitialized(ServletContextEvent arg0)  {
-		nosi.core.db.migration.api.MigrationIGRPInitConfig.start();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
 	}
 	
 }
