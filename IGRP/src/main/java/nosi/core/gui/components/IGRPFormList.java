46808bc521184c531d4becb6f0b97b4c458a45f9
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2024-11-02 21:09:59 -0100

    v.1.8..1102 Maintenance

--- IGRP/src/main/java/nosi/core/gui/components/IGRPFormList.java
+++ IGRP/src/main/java/nosi/core/gui/components/IGRPFormList.java
@@ -114,8 +114,8 @@ public class IGRPFormList extends IGRPSeparatorList {
 			this.xml.writeAttribute("type", "start");
 			for (Field field : this.fields) {
 				if (field.isVisible()) {
-					final String val = IgrpHelper.getValue(null, field.getName());
-					this.genRowField(field, val, val);
+				//	final String val = IgrpHelper.getValue(null, field.getName());
+					this.genRowField(field, "", "");
 				}
 			}
 			this.xml.endElement();
