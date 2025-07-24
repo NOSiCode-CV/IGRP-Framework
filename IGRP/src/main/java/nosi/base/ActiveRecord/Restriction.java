2dd600d76d62d6407152ad9ad8a7a2ad14872c16
Marcos Miguel Barros Brito <marcos.m.brito@nosi.cv>
2025-07-24 20:11:55 -0100

    v.1.8..250724 New Restriction notEquals

--- IGRP/src/main/java/nosi/base/ActiveRecord/Restriction.java
+++ IGRP/src/main/java/nosi/base/ActiveRecord/Restriction.java
@@ -6,7 +6,9 @@ package nosi.base.ActiveRecord;
 public interface Restriction<T> extends CommonRestriction<T> {
 
 	T equals(String name, Object value);
-	
+
+	T notEquals(String name, Object value);
+
 	T like(String name, Object value);
 	
 	T notLike(String name, Object value);
